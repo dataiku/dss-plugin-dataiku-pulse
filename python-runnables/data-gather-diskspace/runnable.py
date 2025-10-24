2f93b14639f7049e3b00368b4c7be27b6487055d
smazzei <Stephen.Mazzei@dataiku.com>
2025-10-24 19:13:15 +0000

    Edited file 'python-runnables/data-gather-diskspace/runnable.py'

--- python-runnables/data-gather-diskspace/runnable.py
+++ python-runnables/data-gather-diskspace/runnable.py
@@ -1,30 +1,124 @@
-# This file is the actual code for the Python runnable data-gather-diskspace
-from dataiku.runnables import Runnable
+from sage.src import dss_funcs, dss_folder
+
+import os
+import subprocess
+import pandas as pd
+from datetime import datetime, date, timedelta
+import logging
+
+from dataiku.runnables import Runnable, ResultTable
+
+def get_size(d):
+    try:
+        result = subprocess.run(f"du -sc {d}", shell=True, capture_output=True, text=True, check=True)
+        size = result.stdout.split("\t")[0]
+        size = int(size)
+    except:
+        size = 0
+    return size
+
 
 class MyRunnable(Runnable):
-    """The base interface for a Python runnable"""
-
     def __init__(self, project_key, config, plugin_config):
-        """
-        :param project_key: the project in which the runnable executes
-        :param config: the dict of the configuration of the object
-        :param plugin_config: contains the plugin settings
-        """
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
+        self.sage_project_key = plugin_config.get("sage_project_key", None)
+        self.sage_project_url = plugin_config.get("sage_project_url", None)
+        self.sage_project_api = plugin_config.get("sage_project_api", None)
+        self.ignore_certs     = plugin_config.get("ignore_certs", False)
+        self.dt = datetime.utcnow()
         
     def get_progress_target(self):
-        """
-        If the runnable will return some progress info, have this function return a tuple of 
-        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
-        """
         return None
 
     def run(self, progress_callback):
-        """
-        Do stuff here. Can return a string or raise an exception.
-        The progress_callback is a function expecting 1 value: current progress
-        """
-        raise Exception("unimplemented")
+        # Get local client and name
+        local_client = dss_funcs.build_local_client()
+        instance_name = dss_funcs.get_dss_name(local_client)
         
+        # change directory and get audit logs
+        root_path = local_client.get_instance_info().raw["dataDirPath"]
+        os.chdir(root_path)
+        
+        # Find directories maxdepth
+        results = []
+        cmd = "find . -maxdepth 3 -type d"
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+        directories = result.stdout.split("\n")
+        directories.remove(".")
+        
+        # turn into a df
+        df = pd.DataFrame(directories, columns=["directory"])
+        
+        # remove jupyter-run / .git -- permission issues with sudo stuff
+        df = df[~df["directory"].str.contains("jupyter-run")]
+        df = df[~df["directory"].str.contains(".git")]
+
+        # Explode directory
+        cols = ["dot", "level_1", "level_2", "level_3"]
+        df[cols] = df["directory"].str.split("/",  expand=True)
+
+        # remove columns
+        del df["dot"]
+        del df["directory"]
+        
+        # Get details on sizes - level_1
+        df["level_1_size"] = 0
+        for i,g in df.groupby(by=["level_1"]):
+            logging.error(f"value i: {i}")
+            size = get_size(i)
+            df.loc[df["level_1"] == i, "level_1_size"] = size
+                        
+        # Filter size on a base number (1gb / adjustable)
+        gb = 1000000 * 1
+        df = df[df["level_1_size"] >= gb]
+
+        # Get details on sizes - level_2
+        df["level_2_size"] = 0
+        for i,g in df.groupby(by=["level_1", "level_2"]):
+            d = "/".join(i)
+            size = get_size(d)
+            df.loc[
+                (df["level_1"] == i[0])
+                & (df["level_2"] == i[1]), "level_2_size"] = size
+
+        # Get details on sizes - level_3
+        df["level_3_size"] = 0
+        for i,g in df.groupby(by=["level_1", "level_2", "level_3"]):
+            d = "/".join(i)
+            size = get_size(d)
+            df.loc[
+                (df["level_1"] == i[0])
+                & (df["level_2"] == i[1])
+                & (df["level_3"] == i[2]), "level_3_size"] = size
+            
+        results.append(["read/parse", True, None])
+        
+        # loop topics and save data
+        remote_client = dss_funcs.build_remote_client(self.sage_project_url, self.sage_project_api, self.ignore_certs)
+        dt_year  = str(self.dt.year)
+        dt_month = str(f'{self.dt.month:02d}')
+        dt_day   = str(f'{self.dt.day:02d}')
+        df["instance_name"] = instance_name
+        try:
+            write_path = f"/{instance_name}/operating_system/diskspace/{dt_year}/{dt_month}/{dt_day}/data.parquet"
+            dss_folder.write_remote_folder_output(self, remote_client, write_path, df)
+            results.append(["write/save", True, None])
+        except Exception as e:
+            results.append(["write/save", False, e])
+        
+        # return results
+        if results:
+            df = pd.DataFrame(results, columns=["step", "result", "message"])
+            df = df.astype(str)
+            rt = ResultTable()
+            n = 1
+            for col in df.columns:
+                rt.add_column(n, col, "STRING")
+                n +=1
+            for index, row in df.iterrows():
+                rt.add_record(row.tolist())
+            return rt
+        else:
+            raise Exception("Something went wrong")
