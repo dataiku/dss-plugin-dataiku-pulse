f3facf3f1d80bb5f5e52d6dde8534b948b0c41a0
smazzei <Stephen.Mazzei@dataiku.com>
2025-10-24 19:14:30 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -1,30 +1,81 @@
-# This file is the actual code for the Python runnable data-gather-project
-from dataiku.runnables import Runnable
+try:
+    from sage.base_data import project_handle as dss_objs
+except:
+    dss_objs = False
+from sage.src import dss_funcs
+
+import dataiku
+import os
+import pandas as pd
+from datetime import datetime
+
+from dataiku.runnables import Runnable, ResultTable
+
 
 class MyRunnable(Runnable):
-    """The base interface for a Python runnable"""
-
     def __init__(self, project_key, config, plugin_config):
-        """
-        :param project_key: the project in which the runnable executes
-        :param config: the dict of the configuration of the object
-        :param plugin_config: contains the plugin settings
-        """
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
+        self.sage_project_key = plugin_config.get("sage_project_key", None)
+        self.sage_project_url = plugin_config.get("sage_project_url", None)
+        self.sage_project_api = plugin_config.get("sage_project_api", None)
+        self.ignore_certs     = plugin_config.get("ignore_certs", False)
+        self.dt = datetime.utcnow()
+        
+        # Set environment variable
+        self.sage_folder_connection = plugin_config.get("sage_folder_connection", "filesystem_folders")
+        os.environ["SAGE_FOLDER_CONNECTION"] = self.sage_folder_connection
         
     def get_progress_target(self):
-        """
-        If the runnable will return some progress info, have this function return a tuple of 
-        (target, unit) where unit is one of: SIZE, FILES, RECORDS, NONE
-        """
         return None
 
     def run(self, progress_callback):
-        """
-        Do stuff here. Can return a string or raise an exception.
-        The progress_callback is a function expecting 1 value: current progress
-        """
-        raise Exception("unimplemented")
+        # Test if modules are found
+        if not dss_objs:
+            raise Exception("No categories or modules found")
+
+        # Build Local Client
+        local_client = dss_funcs.build_local_client()
         
+        # Grab some exra details
+        client_d = {}
+        try:
+            client_d["python_env_name"] = client.get_general_settings().settings["codeEnvs"]["defaultPythonEnv"]
+            if not client_d["python_env_name"]:
+                client_d["python_env_name"] = "USE_BUILTIN_MODE"
+        except:
+            client_d["python_env_name"] = "USE_BUILTIN_MODE"
+        try:
+            client_d["r_env_name"] = client.get_general_settings().settings["codeEnvs"]["defaultREnv"]
+            if not client_d["r_env_name"]:
+                client_d["r_env_name"] = "USE_BUILTIN_MODE"
+        except:
+            client_d["r_env_name"] = "USE_BUILTIN_MODE"
+        try:
+            client["container_env_name"] = client.get_general_settings().settings["containerSettings"]["defaultExecutionConfig"]
+            if not client_d["container_env_name"]:
+                client_d["container_env_name"] = "DSS_LOCAL"
+        except:
+            client_d["container_env_name"] = "DSS_LOCAL"
+        
+        # Collect the modules && Run the modules
+        results = []
+        for key in local_client.list_project_keys():
+            project_handle = local_client.get_project(project_key=key)
+            results += dss_funcs.run_modules(self, dss_objs, project_handle, client_d, key)
+            
+        # return results
+        if results:
+            df = pd.DataFrame(results, columns=["project_key", "path", "module_name", "step", "result", "message"])
+            df = df.astype(str)
+            rt = ResultTable()
+            n = 1
+            for col in df.columns:
+                rt.add_column(n, col, "STRING")
+                n +=1
+            for index, row in df.iterrows():
+                rt.add_record(row.tolist())
+            return rt
+        else:
+            raise Exception("Something went wrong")
