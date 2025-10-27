dd6a289dfcfcae6550f6c825fa49ef923f1f0989
smazzei <Stephen.Mazzei@dataiku.com>
2025-10-27 14:23:59 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -1,8 +1,8 @@
 try:
-    from sage.base_data import project_handle as dss_objs
+    from dataikupulse.base_data import project_handle as dss_objs
 except:
     dss_objs = False
-from sage.src import dss_funcs
+from dataikupulse.src import dss_funcs
 
 import dataiku
 import os
