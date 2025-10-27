af2f4791748981c49983fb8a6b4b4ea5b6bbc882
smazzei <Stephen.Mazzei@dataiku.com>
2025-10-27 14:25:55 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -17,15 +17,15 @@ class MyRunnable(Runnable):
         self.project_key = project_key
         self.config = config
         self.plugin_config = plugin_config
-        self.sage_project_key = plugin_config.get("sage_project_key", None)
-        self.sage_project_url = plugin_config.get("sage_project_url", None)
-        self.sage_project_api = plugin_config.get("sage_project_api", None)
+        self.pulse_project_key = plugin_config.get("pulse_project_key", None)
+        self.pulse_project_url = plugin_config.get("pulse_project_url", None)
+        self.pulse_project_api = plugin_config.get("pulse_project_api", None)
         self.ignore_certs     = plugin_config.get("ignore_certs", False)
         self.dt = datetime.utcnow()
         
         # Set environment variable
-        self.sage_folder_connection = plugin_config.get("sage_folder_connection", "filesystem_folders")
-        os.environ["SAGE_FOLDER_CONNECTION"] = self.sage_folder_connection
+        self.pulse_folder_connection = plugin_config.get("pulse_folder_connection", "filesystem_folders")
+        os.environ["pulse_FOLDER_CONNECTION"] = self.pulse_folder_connection
         
     def get_progress_target(self):
         return None
