18e82c6c380d3263c982961307b78cdec65db339
smazzei <Stephen.Mazzei@dataiku.com>
2025-10-29 19:16:12 +0000

    Edited file 'python-lib/dataikupulse/base_data/client_handle/instance/name_mapping.py'

---
+++ python-lib/dataikupulse/base_data/client_handle/instance/name_mapping.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from dataikupulse.src.dss_funcs import get_dss_name_id_mapping
+
+
+def main(self, client, client_d = {}):
+    mapping = get_dss_name_id_mapping(client)
+    df = pd.DataFrame(
+        [mapping], columns=["instance_name", "instance_name_base", "instance_id_base"]
+    )
+    return df
