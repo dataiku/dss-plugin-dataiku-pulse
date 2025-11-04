eb6827832fb3c494f34afe914210793a04ce5ce6
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-04 16:34:08 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -39,8 +39,8 @@ class MyRunnable(Runnable):
         self.pulse_project_url = plugin_config.get("pulse_project_url", None)
         self.pulse_project_api = plugin_config.get("pulse_project_api", None)
         self.ignore_certs      = plugin_config.get("ignore_certs", False)
-        self.do_parallel       = False
-        self.cores             = 2
+        self.do_parallel       = plugin_config.get("do_parallel", False)
+        self.cores             = plugin_config.get("cores", 2)
         self.dt                = datetime.utcnow()
         
         # Set environment variable
