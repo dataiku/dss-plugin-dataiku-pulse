911af2ea8f216d3e6bd9be97e8de38b049a46b29
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-14 22:57:49 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -80,11 +80,10 @@ class MyRunnable(Runnable):
         project_keys = local_client.list_project_keys()
         if self.do_parallel:
             pk_arrays = np.array_split(project_keys, self.cores)
-            dfs = Parallel(n_jobs=self.cores)(delayed(self.data_gather)(client_d, project_keys)
-                                              for project_keys in pk_arrays)
+            dfs = Parallel(n_jobs=self.cores)(delayed(self.data_gather)(project_keys) for project_keys in pk_arrays)
             df = pd.concat(dfs, ignore_index=True)
         else:
-            df = self.data_gather(client_d, project_keys)            
+            df = self.data_gather(project_keys)            
             
         # return results
         if not df.empty:
