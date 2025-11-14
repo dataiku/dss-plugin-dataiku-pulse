0a7b8b5d9f9cf6158c6bae3be7a62bd4be7cd370
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-14 22:19:40 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from dataiku.runnables import Runnable, ResultTable
 
 
-def data_gather(project_keys):
+def data_gather(self, project_keys):
     results = []
     for key in project_keys:
         project_handle = local_client.get_project(project_key=key)
