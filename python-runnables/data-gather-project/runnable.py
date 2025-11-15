23686e571ba4eb92cd4c73c45f6b3b993b7df668
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-15 01:58:02 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -9,20 +9,7 @@ from datetime import datetime
 
 from dataiku.runnables import Runnable, ResultTable
 
-def data_gather(project_keys):
-    results = [[1,1,1,1,1,1]]
-    #for key in project_keys:
-    #    project_handle = self.local_client.get_project(project_key=key)
-    #    results += dss_funcs.run_modules(self, "projects", project_handle, self.client_d, key)
-    #    break
-    cols = ["project_key", "path", "module_name", "step", "result", "message"]
-    if results:
-        df = pd.DataFrame(results, columns=cols)
-    else:
-        df = pd.DataFrame(columns=cols)
-    return df
-    
-    
+
 class MyRunnable(Runnable):
     def __init__(self, project_key, config, plugin_config):
         self.project_key = project_key
@@ -42,7 +29,18 @@ class MyRunnable(Runnable):
         return None
 
     
-
+    def data_gather(project_keys):
+        results = [[1,1,1,1,1,1]]
+        #for key in project_keys:
+        #    project_handle = self.local_client.get_project(project_key=key)
+        #    results += dss_funcs.run_modules(self, "projects", project_handle, self.client_d, key)
+        #    break
+        cols = ["project_key", "path", "module_name", "step", "result", "message"]
+        if results:
+            df = pd.DataFrame(results, columns=cols)
+        else:
+            df = pd.DataFrame(columns=cols)
+        return df
     
     
     def run(self, progress_callback):
@@ -76,7 +74,7 @@ class MyRunnable(Runnable):
         project_keys = local_client.list_project_keys()
         if self.do_parallel:
             pk_arrays = np.array_split(project_keys, self.cores)
-            dfs = Parallel(n_jobs=self.cores)(delayed(data_gather)(project_keys)
+            dfs = Parallel(n_jobs=self.cores)(delayed(self.data_gather)(project_keys)
                                               for project_keys in pk_arrays)
             df = pd.concat(dfs, ignore_index=True)
         else:
