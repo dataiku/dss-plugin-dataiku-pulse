1a9e0e38b41f2fceda68dca586b9f94dc0eb753b
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-15 02:07:43 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -30,11 +30,11 @@ class MyRunnable(Runnable):
 
     
     def data_gather(self, project_keys):
-        results = [[1,1,1,1,1,1]]
-        #for key in project_keys:
-        #    project_handle = self.local_client.get_project(project_key=key)
-        #    results += dss_funcs.run_modules(self, "projects", project_handle, self.client_d, key)
-        #    break
+        results = []
+        for key in project_keys:
+            project_handle = self.local_client.get_project(project_key=key)
+            results += dss_funcs.run_modules(self, "projects", project_handle, self.client_d, key)
+            break
         cols = ["project_key", "path", "module_name", "step", "result", "message"]
         if results:
             df = pd.DataFrame(results, columns=cols)
