518afd777aea7cd0d7b4044a8e0033a8f4da06d0
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-19 21:42:25 +0000

    Edited file 'python-runnables/data-gather-instance/runnable.py'

--- python-runnables/data-gather-instance/runnable.py
+++ python-runnables/data-gather-instance/runnable.py
@@ -31,6 +31,10 @@ class MyRunnable(Runnable):
 
     def run(self, progress_callback):
         
+        
+        rt = ResultTable()
+        rt.add_column(1, "Column1", "STRING")
+        rt.add_record(["hi"])
         return "hi"
     
     
