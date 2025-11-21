68317ebb8caec6351a699dd298acd41de6f97eba
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-21 20:57:12 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -45,5 +45,5 @@ class MyRunnable(Runnable):
                 }
             
             # Create a remote client
-            remote_client = dss_funcs.build_remote_client(worker_url, worker_api, self.params["worker_hosts"]ignore_certs)
+            remote_client = dss_funcs.build_remote_client(worker_url, worker_api, preset_pc["ignore_certs"])
             
