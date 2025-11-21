33c5574bfe577d3d059f516c9e2d224cff44e0af
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-21 21:12:19 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -43,8 +43,7 @@ class MyRunnable(Runnable):
                     self.preset_pc = preset.plugin_config["macro_configs"]
                 except:
                     pass
-                
             
             # Create a remote client
-            #remote_client = dss_funcs.build_remote_client(worker_url, worker_api, self.preset_pc["ignore_certs"])
+            remote_client = dss_funcs.build_remote_client(worker_url, worker_api, self.preset_pc["ignore_certs"])
             
