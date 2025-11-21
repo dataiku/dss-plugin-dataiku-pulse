b1900568a403be6f95f4f59d5386ac0a2299c2f9
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-21 22:40:42 +0000

    Edited file 'python-lib/dataikupulse/src/dss_init.py'

--- python-lib/dataikupulse/src/dss_init.py
+++ python-lib/dataikupulse/src/dss_init.py
@@ -25,6 +25,8 @@ def update_plugin_config(self, plugin_handle):
     param_set.save()
     # Node
     param_set = settings.get_parameter_set(parameter_set_name="params-worker-instances")
+    for preset_name in param_set.list_preset_names():
+        param_set.delete_preset(preset_name=preset_name)
     preset = param_set.get_preset(preset_name=self.preset_pc_name)
     if not preset:
         preset = param_set.create_preset(preset_name=self.preset_pc_name)
