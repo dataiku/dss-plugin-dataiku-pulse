b616cf43d1d0617ee7ebefadff7252a25b7b2cb9
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-21 21:46:37 +0000

    Edited file 'python-lib/dataikupulse/src/dss_init.py'

--- python-lib/dataikupulse/src/dss_init.py
+++ python-lib/dataikupulse/src/dss_init.py
@@ -12,26 +12,6 @@ def load_yaml(path="./scenarios.yaml"):
         config = {}
     return config
 
-def update_plugin_config(self, plugin_handle):
-    settings = plugin_handle.get_settings()
-    settings.settings["defaultPermission"] = {"admin": True, "canViewComponents": False}
-    settings.settings["excludedFromCDE"] = True
-    settings.settings["detailsNotVisible"] = False 
-    settings.settings["codeEnvName"] = "plugin_dataiku-pulse_managed"
-    settings.settings["config"]["pulse_repo_url"]    = self.pulse_repo_url
-    settings.settings["config"]["pulse_repo_branch"] = self.pulse_repo_branch 
-    settings.settings["config"]["pulse_project_key"]   = self.pulse_project_key
-    settings.settings["config"]["pulse_project_url"]   = self.pulse_project_url
-    settings.settings["config"]["pulse_project_api"]   = self.pulse_project_api 
-    settings.settings["config"]["pulse_worker_key"]    = self.pulse_worker_key
-    settings.settings["config"]["pulse_dataiku_user"]  = self.pulse_dataiku_user 
-    settings.settings["config"]["ignore_certs"]       = self.ignore_certs
-    settings.settings["config"]["pulse_folder_connection"] = self.pulse_folder_connection
-    settings.settings["config"]["do_parallel"] = self.do_parallel
-    settings.settings["config"]["cores"] = self.cores
-    settings.save()
-    return
-
     
 def install_plugin(self, remote_client):
     # Only install if not found, if found and set to update, patch
@@ -39,11 +19,11 @@ def install_plugin(self, remote_client):
     for plugin in remote_client.list_plugins():
         if plugin["id"] == "dataiku-pulse":
             pulse_found = True
-    if pulse_found and self.update_github:
+    if pulse_found and self.config["update_github"]:
         plugin_handle = remote_client.get_plugin(plugin_id="dataiku-pulse")
         git_update = plugin_handle.update_from_git(
-            repository_url=self.pulse_repo_url,
-            checkout=self.pulse_repo_branch
+            repository_url = self.params["pulse_repo_url"],
+            checkout = self.params["pulse_repo_branch"]
         )
         r = git_update.wait_for_result()
         if not r["success"]:
