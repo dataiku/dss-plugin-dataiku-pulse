e86bd32d0b00064e9400579baf167bb181228191
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-21 21:07:25 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -30,7 +30,7 @@ class MyRunnable(Runnable):
             preset_name  = worker_host["preset_name"]
             
             # Get the respective param_set if available
-            param_set = settings.get_parameter_set(parameter_set_name="params-worker-instances")
+            param_set = plugin_settings.get_parameter_set(parameter_set_name="params-worker-instances")
             preset = param_set.get_preset(preset_name=preset_name)
             try:
                 preset_pc = preset.plugin_config["macro_configs"]
