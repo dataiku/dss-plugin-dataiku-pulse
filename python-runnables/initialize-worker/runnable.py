4ba8d7e3d66520ffa92f54865b733226fc429c41
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-21 21:19:27 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -54,7 +54,7 @@ class MyRunnable(Runnable):
             
             # Install/Update Plugin if not found
             if cont:
-                if self.pulse_project_url != worker_url:
+                if self.params["pulse_project_url"] != worker_url:
                     try:
                         dss_init.install_plugin(self, remote_client)
                         results.append([worker_url, "Plugin Configured", True, None])
