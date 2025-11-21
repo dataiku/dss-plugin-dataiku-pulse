0ed415da82f6948ac988b422b21f78c355b884bc
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-21 21:09:40 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -33,9 +33,9 @@ class MyRunnable(Runnable):
             param_set = plugin_settings.get_parameter_set(parameter_set_name="params-worker-instances")
             preset = param_set.get_preset(preset_name=preset_name)
             try:
-                preset_pc = preset.plugin_config["macro_configs"]
+                self.preset_pc = preset.plugin_config["macro_configs"]
             except:
-                preset_pc = {
+                self.preset_pc = {
                     "pulse_dataiku_user": "admin",
                     "ignore_certs": False,
                     "do_parallel": False,
@@ -43,6 +43,7 @@ class MyRunnable(Runnable):
                     "macro_configs": [],
                     
                 }
+            
             
             # Create a remote client
             remote_client = dss_funcs.build_remote_client(worker_url, worker_api, preset_pc["ignore_certs"])
