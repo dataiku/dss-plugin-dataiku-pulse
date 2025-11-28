2d994b1600342a54c233cf88825da59aec74f1c9
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 18:37:15 +0000

    Edited file 'python-lib/dataikupulse/src/dss_folder.py'

--- python-lib/dataikupulse/src/dss_folder.py
+++ python-lib/dataikupulse/src/dss_folder.py
@@ -70,8 +70,8 @@ def write_local_folder_output(self, project_handle, folder_name, path, df):
 
 
 # ---------- DATAIKU REMOTE FOLDERS --------------------------------------------------------
-def write_remote_folder_output(self, client, path, df):
-    project_handle = client.get_project(project_key=self.pulse_project_key)
+def write_remote_folder_output(self, path, df):
+    project_handle = self.remote_client.get_project(project_key=self.pulse_project_key)
     fid = None
     for f in project_handle.list_managed_folders():
         if f["name"] == "partitioned_data":
