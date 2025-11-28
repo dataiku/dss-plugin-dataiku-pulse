f577a2569dce9aa1d54371f012092da447630db8
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 19:47:29 +0000

    Edited file 'python-lib/dataikupulse/src/dss_funcs.py'

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -99,7 +99,7 @@ def run_modules(self, mode, handle, client_d = {}, project_key = None):
                 continue # nothing to write, skip
             try:
                 # Remote client and DT parsing
-                remote_client = build_remote_client(self.pulse_project_url, self.pulse_project_api, self.ignore_certs)
+                remote_client = build_remote_client(self)
                 dt_year  = str(self.dt.year)
                 dt_month = str(f'{self.dt.month:02d}')
                 dt_day   = str(f'{self.dt.day:02d}')
