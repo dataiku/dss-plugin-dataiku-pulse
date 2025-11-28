94869c89ee288608ea827b0295a8efa0390b0dd7
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 20:24:07 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -11,10 +11,6 @@ class MyRunnable(Runnable):
         self.config = config
         self.plugin_config = plugin_config
         self.params = plugin_config.get("pulse_primary", {})
-        self.preset_pc = dss_funcs.get_preset_pc("DATAIKU-PULSE")
-        self.local_client = dss_funcs.build_local_client()
-        self.remote_client = dss_funcs.build_remote_client(self)
-        self.dt = datetime.utcnow()
         
         logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.ERROR)
         self.logger = logging.getLogger(__name__)
