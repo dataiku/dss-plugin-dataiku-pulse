7aa03d57ba213ee895c222916d91e9f89b7505ea
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 19:54:13 +0000

    Edited file 'python-runnables/data-gather-project/runnable.py'

--- python-runnables/data-gather-project/runnable.py
+++ python-runnables/data-gather-project/runnable.py
@@ -1,13 +1,12 @@
-from dataikupulse.src import dss_funcs
-
-import dataiku
-import pandas as pd
-import numpy as np
-import os
-from joblib import Parallel, delayed
-from datetime import datetime
-
 from dataiku.runnables import Runnable, ResultTable
+from dataikupulse.src import dss_funcs
+from datetime import datetime
+from joblib import Parallel, delayed
+import dataiku
+import numpy as np
+import pandas as pd
+import os
+
 
 
 class MyRunnable(Runnable):
