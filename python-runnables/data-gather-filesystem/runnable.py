0c4ec06f67a024d46d93a90ab8e92524d75d7e61
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 19:38:32 +0000

    Edited files

--- python-runnables/data-gather-filesystem/runnable.py
+++ python-runnables/data-gather-filesystem/runnable.py
@@ -53,7 +53,7 @@ class MyRunnable(Runnable):
         df["timestamp"] = self.dt
         try:
             write_path = f"/{instance_name}/operating_system/filesystem/{dt_year}/{dt_month}/{dt_day}/data.parquet"
-            dss_folder.write_remote_folder_output(self, remote_client, write_path, df)
+            dss_folder.write_remote_folder_output(self, write_path, df)
             results.append(["write/save", True, None])
         except Exception as e:
             results.append(["write/save", False, e])
