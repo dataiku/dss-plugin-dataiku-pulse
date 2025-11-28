d54eba9b9960381194e111de2400fed126da7943
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 20:12:47 +0000

    Edited file 'python-lib/dataikupulse/base_data/audit_log/event_mapping.py'

--- python-lib/dataikupulse/base_data/audit_log/event_mapping.py
+++ python-lib/dataikupulse/base_data/audit_log/event_mapping.py
@@ -84,7 +84,7 @@ def main(self, df):
             grp = grp.dropna(axis=1, how='all').reset_index(drop=True)
             try:
                 write_path = f"/{instance_name}/dataiku_usage/{category}/{dt_year}/{dt_month}/{dt_day}/data-{dt_epoch}.parquet"
-                dss_folder.write_remote_folder_output(self, remote_client, write_path, grp)
+                dss_folder.write_remote_folder_output(self, write_path, grp)
                 results.append([f"write/save - Dataiku Usage {category}", True, f"data-{dt_epoch}.parquet"])
             except Exception as e:
                 results.append([f"write/save - Dataiku Usage {category}", False, e])
