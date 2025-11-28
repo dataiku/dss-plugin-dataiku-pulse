812274017d645d495bafba6e640cc3e9bb0f7435
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 21:06:20 +0000

    Edited file 'python-runnables/initialize-worker/runnable.py'

--- python-runnables/initialize-worker/runnable.py
+++ python-runnables/initialize-worker/runnable.py
@@ -26,7 +26,7 @@ class MyRunnable(Runnable):
             worker_url = worker_host.get("worker_url", None)
             worker_api = worker_host.get("worker_api", None)
             preset_name = worker_host.get("preset_name", None)
-            self.preset_pc = dss_funcs.get_preset_pc(preset_name)
+            self.preset_pc = dss_funcs.get_preset_pc(self, preset_name)
             self.preset_pc_name = "DATAIKU-PULSE"
             
             # Create a remote client
