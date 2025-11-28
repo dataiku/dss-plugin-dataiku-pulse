e6c769145357655991a89e181608cff28b70ea4d
smazzei <Stephen.Mazzei@dataiku.com>
2025-11-28 18:09:54 +0000

    Edited file 'python-runnables/data-gather-diskspace/runnable.py'

--- python-runnables/data-gather-diskspace/runnable.py
+++ python-runnables/data-gather-diskspace/runnable.py
@@ -17,7 +17,7 @@ def get_size(d):
         size = 0
     return size
 
-
+        
 class MyRunnable(Runnable):
     def __init__(self, project_key, config, plugin_config):
         self.project_key = project_key
