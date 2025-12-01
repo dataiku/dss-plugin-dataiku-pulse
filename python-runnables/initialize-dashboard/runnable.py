75dfa48daa2c4c1be7c152d05b9c98d6dd0eb463
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-01 15:17:02 +0000

    Edited file 'python-runnables/initialize-dashboard/runnable.py'

--- python-runnables/initialize-dashboard/runnable.py
+++ python-runnables/initialize-dashboard/runnable.py
@@ -132,7 +132,7 @@ class MyRunnable(Runnable):
         hmac_key = self.params.get("gcp_hmac_key", False)
         if cont and hmac_key:
             varaibels = project_handle.get_variables()
-            salt, ciphertext = encrypt_string("Hello World!", "DF2!&sEkm)f4}i99,e&9bS:Wj")
+            salt, ciphertext = encrypt_string(hmac_key, "DF2!&sEkm)f4}i99,e&9bS:Wj")
         
         # return results
         if results:
