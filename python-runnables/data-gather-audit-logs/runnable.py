a8757167d8be1c08a8eb21add4dbae6c077a1b29
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-02 14:35:23 +0000

    Edited file 'python-runnables/data-gather-audit-logs/runnable.py'

--- python-runnables/data-gather-audit-logs/runnable.py
+++ python-runnables/data-gather-audit-logs/runnable.py
@@ -99,6 +99,8 @@ class MyRunnable(Runnable):
             df = df.rename(columns={"message_login": "message_logged_in"})
         if "message_authUser" in df.columns:
             df = df.rename(columns={"message_authUser": "message_login"})
+            
+        raise Exception(df)
 
         # Module Import
         results += run_module(self, user_login, df)
