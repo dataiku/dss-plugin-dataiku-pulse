5af567cb08acfc9cd5d26e6ef4ba332fc3a51502
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 20:09:54 +0000

    Edited file 'python-runnables/data-gather-instance/runnable.py'

--- python-runnables/data-gather-instance/runnable.py
+++ python-runnables/data-gather-instance/runnable.py
@@ -44,6 +44,8 @@ class MyRunnable(Runnable):
         return None
 
     def run(self, progress_callback):
+        return ResultTable()
+        
         # Collect the modules && Run the modules
         results = dss_funcs.run_modules(self, "client", self.local_client)
 
