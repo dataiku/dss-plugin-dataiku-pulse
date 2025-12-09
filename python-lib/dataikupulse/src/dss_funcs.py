5f5bf6804bdf284be4b46206fbb9c8534fd7471b
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 20:27:06 +0000

    Edited files

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -119,7 +119,6 @@ def run_modules(self, mode, project_handle = None, client_d = {}, project_key = 
                 instance_name = self.instance_name
                 if "instance_name" not in df.columns:
                     df["instance_name"] = instance_name
-                apple
                 write_path = f"{instance_name}/{path}/{module_name}/{dt_year}/{dt_month}/{dt_day}/data.parquet"
                 if project_key:
                     write_path = f"{instance_name}/{path}/{module_name}/{dt_year}/{dt_month}/{dt_day}/{project_key}_data.parquet"
