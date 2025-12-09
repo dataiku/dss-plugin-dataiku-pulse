9f968d951b7efb6f82ebfc442051f68cf51dd7ab
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 17:30:24 +0000

    Edited file 'python-runnables/data-gather-instance/runnable.py'

--- python-runnables/data-gather-instance/runnable.py
+++ python-runnables/data-gather-instance/runnable.py
@@ -28,11 +28,9 @@ class MyRunnable(Runnable):
     def run(self, progress_callback):
         # Collect the modules && Run the modules
         results = dss_funcs.run_modules(self, "client", self.local_client)
-        self.logger.error("Finished collecting Instance level Data")
         
         # return results
         if results:
-            self.logger.error("Creating results DF")
             df = pd.DataFrame(results, columns=["instance_level", "path", "module_name", "step", "result", "message"])
             del df["instance_level"]
             df = df.astype(str)
@@ -43,8 +41,6 @@ class MyRunnable(Runnable):
                 n +=1
             for index, row in df.iterrows():
                 rt.add_record(row.tolist())
-            self.logger.error("Returning results")
-            raise Exception()
             return rt
         else:
             raise Exception("Something went wrong")
