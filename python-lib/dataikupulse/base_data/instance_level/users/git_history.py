ce67085c2a38ea31af1f9afaa8ad53bb5b373edf
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 20:16:06 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/users/git_history.py'

--- python-lib/dataikupulse/base_data/instance_level/users/git_history.py
+++ python-lib/dataikupulse/base_data/instance_level/users/git_history.py
@@ -22,8 +22,8 @@ def split_work(client, project_keys):
     return pd.concat(dfs, ignore_index=True)
 
 
-def main(self, client, client_d = {}):
-    project_keys = client.list_project_keys()
+def main(self):
+    project_keys = self.local_client.list_project_keys()
     pkey_array = np.array_split(project_keys, 2)
     results = Parallel(n_jobs=2)(delayed(split_work)(client=client, project_keys=i) for i in pkey_array)
     df = pd.concat(results, ignore_index=True)
