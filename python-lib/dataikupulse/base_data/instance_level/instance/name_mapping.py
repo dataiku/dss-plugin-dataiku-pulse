396bd65a0254e0b1b2db4b2dbd7485c326a76ce9
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 20:50:51 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/instance/name_mapping.py'

--- python-lib/dataikupulse/base_data/instance_level/instance/name_mapping.py
+++ python-lib/dataikupulse/base_data/instance_level/instance/name_mapping.py
@@ -3,6 +3,7 @@ from dataikupulse.src.dss_funcs import get_dss_name_id_mapping
 
 
 def main(self):
+    return df.pd.DataFrame()
     mapping = get_dss_name_id_mapping(self)
     df = pd.DataFrame(
         [mapping], columns=["instance_name", "instance_name_base", "instance_id_base"]
