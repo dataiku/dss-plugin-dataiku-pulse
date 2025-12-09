e7e473956de6092761094468f2154a88bd10e1b0
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 21:03:57 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/users/git_history.py'

--- python-lib/dataikupulse/base_data/instance_level/users/git_history.py
+++ python-lib/dataikupulse/base_data/instance_level/users/git_history.py
@@ -1,14 +1,16 @@
 import dataiku
 import pandas as pd
-from datetime import date
+import numpy as np
+from joblib import Parallel, delayed
+from datetime import datetime, date, timedelta
+
 
 today = date.today()
 
-def main(self):
-    project_keys = self.local_client.list_project_keys()
+def split_work(client, project_keys):
     dfs = []
     for project_key in project_keys:
-        project_handle = self.local_client.get_project(project_key=project_key)
+        project_handle = client.get_project(project_key=project_key)
         git_log = project_handle.get_project_git().log()
         df = pd.DataFrame(git_log["entries"])
         if df.empty:
@@ -17,5 +19,12 @@ def main(self):
         df = df[(df["timestamp"].dt.date >= today)]
         df["project_key"] = project_key
         dfs.append(df)
-    df = pd.concat(dfs, ignore_index=True)
+    return pd.concat(dfs, ignore_index=True)
+
+
+def main(self, client, client_d = {}):
+    project_keys = client.list_project_keys()
+    pkey_array = np.array_split(project_keys, 2)
+    results = Parallel(n_jobs=2)(delayed(split_work)(client=client, project_keys=i) for i in pkey_array)
+    df = pd.concat(results, ignore_index=True)
     return df
