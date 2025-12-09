6388526bdfe1591a809e4a0bcb721cfb004799e5
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 20:15:36 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/llms/metadata.py'

--- python-lib/dataikupulse/base_data/instance_level/llms/metadata.py
+++ python-lib/dataikupulse/base_data/instance_level/llms/metadata.py
@@ -2,8 +2,8 @@ import pandas as pd
 from dataikupulse.src import dss_funcs
 
 
-def main(self, client, client_d = {}):
-    project_handle = client.get_project(self.params["pulse_worker_key"])
+def main(self):
+    project_handle = self.local_client.get_project(self.params["pulse_worker_key"])
     if not project_handle.list_llms():
         return pd.DataFrame()
     
