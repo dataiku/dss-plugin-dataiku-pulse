79385aa0ef7da873cfc2f97cd775512f6e809824
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 20:13:22 +0000

    Edited file 'python-lib/dataikupulse/src/dss_funcs.py'

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -98,7 +98,10 @@ def run_modules(self, mode, project_handle = None, client_d = {}, project_key = 
                 module = importlib.util.module_from_spec(spec)
                 spec.loader.exec_module(module)
                 if hasattr(module, 'main'):
-                    df = module.main(self, project_handle, client_d)
+                    if project_handle:
+                        df = module.main(self, project_handle, client_d)
+                    else:
+                        df = module.main(self)
                     results.append([project_key, path, module_name, "load/run", True, None])
             except Exception as e:
                 df = pd.DataFrame()
