a52e2f5119787d10f1d0e47b725a6826cff1a3cc
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-09 20:14:05 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/instance/connections.py'

--- python-lib/dataikupulse/base_data/instance_level/instance/connections.py
+++ python-lib/dataikupulse/base_data/instance_level/instance/connections.py
@@ -29,10 +29,10 @@ reverse_lookup = {
 
 
 def main(self):
-    connections = client.list_connections_names(connection_type="all")
+    connections = self.local_client.list_connections_names(connection_type="all")
     dfs = []
     for conn in connections:
-        conn_handle = client.get_connection(name=conn)
+        conn_handle = self.local_client.get_connection(name=conn)
         settings = conn_handle.get_settings()
         d = settings.settings
         dfs.append(pd.json_normalize(d))
