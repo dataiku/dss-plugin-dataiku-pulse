bc4775a363303eed51ba8484b161788d0d580745
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-10 21:34:16 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/users/git_history.py'

--- python-lib/dataikupulse/base_data/instance_level/users/git_history.py
+++ python-lib/dataikupulse/base_data/instance_level/users/git_history.py
@@ -22,7 +22,7 @@ def split_work(client, project_keys):
     return pd.concat(dfs, ignore_index=True)
 
 
-def main(self, client, client_d = {}):
+def main(self):
     project_keys = client.list_project_keys()
     pkey_array = np.array_split(project_keys, 2)
     results = Parallel(n_jobs=2, prefer="threads")(
