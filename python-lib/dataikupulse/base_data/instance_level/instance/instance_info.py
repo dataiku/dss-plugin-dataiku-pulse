6981b6e2156016965b771bc56abed2e58285883b
admin <admin>
2025-12-15 21:23:04 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py'

--- python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
+++ python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
@@ -2,22 +2,30 @@ import pandas as pd
 from dataikupulse.src import dss_funcs
 
 FLAT_COLUMNS = {
+    # Identity
     "nodeId",
     "nodeName",
     "nodeType",
     "rawNodeType",
     "hostname",
+
+    # Instance / License
     "installId",
     "dipInstanceId",
     "licenseInstanceId",
     "licenseId",
+
+    # Platform / Versioning
     "dssVersion",
     "os",
     "osVersion",
     "javaVendor",
     "javaVersion",
+
+    # Time (convert upstream if possible)
     "dssStartupTimestamp",
 }
+
 
 def main(self):
     df = pd.json_normalize(self.local_client.get_instance_info().raw)
