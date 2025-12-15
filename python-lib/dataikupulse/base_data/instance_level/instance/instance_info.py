4f0aaa41a18438daafee83eef8f9ccdc08d3dfd0
admin <admin>
2025-12-15 21:49:15 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py'

--- python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
+++ python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
@@ -30,5 +30,5 @@ FLAT_COLUMNS = {
 def main(self):
     df = pd.json_normalize(self.local_client.get_instance_info().raw)
     df["dssStartupTimestamp"] = pd.to_datetime(df["dssStartupTimestamp"], unit='ms')
-    df = dss_funcs.normalize_dataframe("mazzei_pulse", df, FLAT_COLUMNS)
+    df = dss_funcs.normalize_dataframe(self, df, FLAT_COLUMNS)
     return df
