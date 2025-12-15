db6db34a0aba9260751cce33a387917cb25a28fb
admin <admin>
2025-12-15 21:08:23 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py'

--- python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
+++ python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
@@ -31,10 +31,6 @@ RENAME_MAP = {
 
 def main(self):
     df = pd.json_normalize(self.local_client.get_instance_info().raw)
-    for c in ["dssStartupTimestamp"]:
-        df[c] = pd.to_datetime(df[c], unit="ms", utc=True)
-        df[c] = pd.to_datetime(df[c], utc=True)
-        df[c] = df[c].fillna(pd.to_datetime("1970-01-01", utc=True))
-        df[c] = df[c].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+    df["dssStartupTimestamp"] = pd.to_datetime(df["dssStartupTimestamp"], unit='ms')
     df = dss_funcs.normalize_dataframe("mazzei_pulse", df, FLAT_COLUMNS, RENAME_MAP)
     return df
