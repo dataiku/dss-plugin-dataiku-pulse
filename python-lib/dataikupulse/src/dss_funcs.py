995bacc42574d010cf0f2eca3ef3d2a27915b374
admin <admin>
2025-12-16 17:37:39 +0000

    Edited file 'python-lib/dataikupulse/src/dss_funcs.py'

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -204,7 +204,8 @@ def run_modules(self, mode = "instance", project_handle = None, client_d = {}, p
                 write_path = f"{category}/{module_name}/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}"
                 file_name = "data.parquet" 
                 if project_key:
-                    write_path = f"{category}/{module_name}/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/{project_key}_data.parquet"
+                    file_name = f"{project_key}_data.parquet"
+                write_path = f"{write_path}/{file_name}"
                 dss_folder.write_remote_folder_output(self, write_path, df)
                 results.append([project_key, category, module_name, "write/save", True, None])
             except Exception as e:
