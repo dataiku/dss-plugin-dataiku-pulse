6170cafd80dc7a43dc6a1b4174a2a52ebc56aac5
admin <admin>
2025-12-16 21:55:36 +0000

    Edited file 'python-runnables/data-gather-diskspace/runnable.py'

--- python-runnables/data-gather-diskspace/runnable.py
+++ python-runnables/data-gather-diskspace/runnable.py
@@ -120,10 +120,10 @@ class MyRunnable(Runnable):
             else:
                 write_path = f"{layer}/{category}/{module_name}/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/{file_name}"
                 dss_folder.write_remote_folder_output(self, write_path, df)
-                results.append([project_key, category, module_name, f"write/save -- {layer}", True, None])
+            results.append([f"write/save -- {layer}", True, None])
         except Exception as e:
             layer = "raw_errors"
-            results.append([project_key, category, module_name, f"write/save -- QUALITY", False, e])
+            results.append([f"write/save -- QUALITY", False, e])
             
         # return results
         if results:
