ec890262331b631a2ba7d1c905aeb2bcff605140
admin <admin>
2025-12-16 21:28:01 +0000

    Edited file 'python-lib/dataikupulse/src/dss_funcs.py'

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -353,10 +353,12 @@ def run_modules(self, mode = "instance", project_handle = None, client_d = {}, p
             except Exception as e:
                 results.append([project_key, category, module_name, "write/save", False, e])
             try:
+                layer = "silver"
                 df = dss_silver.coerce_schema(df)
                 dq = dss_silver.data_quality(df)
                 if dq["errors"]:
-    
+                    layer = "raw_errors"
+                write_path = f"{layer}/{category}/{module_name}/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/{file_name}"
     return results
 
 
