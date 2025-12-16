04f67d3678b347dad65e748897443e49fc917537
admin <admin>
2025-12-16 15:50:22 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/knowledge_banks/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/knowledge_banks/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/knowledge_banks/metadata.py
@@ -1,11 +1,39 @@
 import pandas as pd
 from dataikupulse.src import dss_funcs
 
+FLAT_COLUMNS = {
+    # Knowledge bank identity
+    "knowledge_banks_projectKey",
+    "knowledge_banks_id",
+    "knowledge_banks_name",
+
+    # Core classification
+    "knowledge_banks_retrieverType",
+    "knowledge_banks_vectorStoreType",
+    "knowledge_banks_embeddingLLMId",
+
+    # Execution / environment
+    "knowledge_banks_rebuildBehavior",
+    "knowledge_banks_envSelection.envMode",
+    "knowledge_banks_envSelection.envName",
+    "knowledge_banks_containerExecSelection.containerMode",
+
+    # Versioning / lifecycle
+    "knowledge_banks_versionTag.versionNumber",
+    "knowledge_banks_versionTag.lastModifiedOn",
+    "knowledge_banks_versionTag.lastModifiedBy.login",
+    "knowledge_banks_creationTag.versionNumber",
+    "knowledge_banks_creationTag.lastModifiedOn",
+    "knowledge_banks_creationTag.lastModifiedBy.login",
+}
+
 
 def main(self, project_handle, client_d = {}):
-    if not project_handle.list_knowledge_banks():
+    try:
+        if not project_handle.list_knowledge_banks():
+            return pd.DataFrame()
+    except:
         return pd.DataFrame()
-    
     prefix = "knowledge_banks"
     df = pd.json_normalize(project_handle.list_knowledge_banks()).add_prefix(f"{prefix}_")
     df = dss_funcs.rename_and_move_first(project_handle, df, f"{prefix}_projectKey", "project_key")
