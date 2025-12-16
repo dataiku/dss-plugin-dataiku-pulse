1422275061f7fed7649e93b0f87fc31a051042c7
admin <admin>
2025-12-16 18:19:27 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/datasets/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/datasets/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/datasets/metadata.py
@@ -30,6 +30,6 @@ def main(self, project_handle, client_d = {}):
         return pd.DataFrame()
     prefix = "dataset_"
     df = pd.json_normalize(project_handle.list_datasets()).add_prefix(prefix)
-    df = dss_funcs.rename_and_move_first(project_handle, df, f"{prefix}projectKey", "project_key")
+    df = dss_funcs.rename_and_move_first(df, f"{prefix}projectKey", "project_key")
     df = dss_funcs.normalize_dataframe(self, df, FLAT_COLUMNS)
     return df
