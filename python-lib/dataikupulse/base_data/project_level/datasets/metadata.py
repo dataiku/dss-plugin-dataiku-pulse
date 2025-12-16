960d4d30d8b6087e9e6fce01c87a5863eec32f3b
admin <admin>
2025-12-16 15:44:41 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/datasets/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/datasets/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/datasets/metadata.py
@@ -1,6 +1,26 @@
 import pandas as pd
 from dataikupulse.src import dss_funcs
 
+FLAT_COLUMNS = {
+    # Dataset identity
+    "dataset_projectKey",
+    "dataset_name",
+    "dataset_smartName",
+
+    # Core classification
+    "dataset_type",
+    "dataset_managed",
+    "dataset_featureGroup",
+    "dataset_typeSystemVersion",
+
+    # Versioning / lifecycle
+    "dataset_versionTag.versionNumber",
+    "dataset_versionTag.lastModifiedOn",
+    "dataset_versionTag.lastModifiedBy.login",
+    "dataset_creationTag.versionNumber",
+    "dataset_creationTag.lastModifiedOn",
+    "dataset_creationTag.lastModifiedBy.login",
+}
 
 def main(self, project_handle, client_d = {}):
     try:
@@ -11,5 +31,5 @@ def main(self, project_handle, client_d = {}):
     prefix = "dataset_"
     df = pd.json_normalize(project_handle.list_datasets()).add_prefix(prefix)
     df = dss_funcs.rename_and_move_first(project_handle, df, f"{prefix}projectKey", "project_key")
-        
+    df = dss_funcs.normalize_dataframe(self, df, FLAT_COLUMNS)
     return df
