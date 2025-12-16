c468e1d20c9c25385ac77daaaf707d3fa47c0fa5
admin <admin>
2025-12-16 21:55:08 +0000

    Edited file 'python-runnables/data-gather-filesystem/runnable.py'

--- python-runnables/data-gather-filesystem/runnable.py
+++ python-runnables/data-gather-filesystem/runnable.py
@@ -74,7 +74,7 @@ class MyRunnable(Runnable):
             else:
                 write_path = f"{layer}/{category}/{module_name}/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/{file_name}"
                 dss_folder.write_remote_folder_output(self, write_path, df)
-                results.append([f"write/save -- {layer}", True, None])
+            results.append([f"write/save -- {layer}", True, None])
         except Exception as e:
             layer = "raw_errors"
             results.append([f"write/save -- QUALITY", False, e])
