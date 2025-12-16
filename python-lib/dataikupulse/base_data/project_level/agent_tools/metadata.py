18a0df1e3dbefa6b081eff83b1c8737ced9146c8
admin <admin>
2025-12-16 15:25:43 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/agent_tools/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/agent_tools/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/agent_tools/metadata.py
@@ -10,5 +10,5 @@ def main(self, project_handle, client_d = {}):
         return pd.DataFrame()
     prefix = "agent_tools"
     df = pd.json_normalize(project_handle.list_agent_tools()).add_prefix(f"{prefix}_")
-    df = dss_funcs.rename_and_move_first(project_handle, df, f"{prefix}_projectKey", "project_key")
+    df = dss_funcs.rename_and_move_first(df, f"{prefix}_projectKey", "project_key")
     return df
