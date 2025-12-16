dbb3039710c5f0a6e186c9dcbac0f9d579e35d76
admin <admin>
2025-12-16 21:27:05 +0000

    Edited file 'python-lib/dataikupulse/src/dss_funcs.py'

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -353,8 +353,8 @@ def run_modules(self, mode = "instance", project_handle = None, client_d = {}, p
             except Exception as e:
                 results.append([project_key, category, module_name, "write/save", False, e])
             try:
-                df = dss_quality.coerce_schema(df)
-                dq = dss_quality.data_quality(df)
+                df = dss_silver.coerce_schema(df)
+                dq = dss_silver.data_quality(df)
                 if dq["errors"]:
     
     return results
