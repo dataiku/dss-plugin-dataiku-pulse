9f938b7319a6549c7d28f15bd53510cdc380be1c
admin <admin>
2025-12-17 01:47:00 +0000

    Edited file 'python-lib/dataikupulse/src/dss_funcs.py'

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -225,6 +225,8 @@ def run_modules(self, mode = "instance", project_handle = None, client_d = {}, p
                 else:
                     write_path = f"{layer}/{category}/{module_name}/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/{file_name}"
                     dss_folder.write_remote_folder_output(self, write_path, df)
+                    write_path = f"{layer}/{category}/{module_name}/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/dq_{file_name}"
+                    dss_folder.write_remote_folder_output(self, write_path, pd.DataFrame(dq))
                     results.append([project_key, category, module_name, f"write/save -- {layer}", True, None])  
             except Exception as e:
                 results.append([project_key, category, module_name, f"write/save -- QUALITY", False, e])
