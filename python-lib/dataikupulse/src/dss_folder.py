3d79ab32e17742fd3ebbd2d9eb796d48a6568ff4
admin <admin>
2025-12-17 02:12:05 +0000

    Edited file 'python-lib/dataikupulse/src/dss_folder.py'

--- python-lib/dataikupulse/src/dss_folder.py
+++ python-lib/dataikupulse/src/dss_folder.py
@@ -78,6 +78,6 @@ def write_remote_folder_output(self, path, df):
         raise Exception()
     folder = project_handle.get_managed_folder(odb_id=fid)
     if "datasets" in path:
-        raise Exception(df.dtypes)
+        raise Exception(df)
     r = folder.put_file(path, df.to_parquet(compression="gzip", engine='pyarrow', index=False))
     return
