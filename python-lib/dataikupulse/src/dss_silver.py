4a9e55d9f23dd3775812a9483eb958ea7bf1b3f6
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-18 21:05:44 +0000

    Edited file 'python-lib/dataikupulse/src/dss_silver.py'

--- python-lib/dataikupulse/src/dss_silver.py
+++ python-lib/dataikupulse/src/dss_silver.py
@@ -200,7 +200,7 @@ def data_quality(df: pd.DataFrame) -> dict:
     # -------------------------
     # 2. Identifier columns must not be numeric
     # -------------------------
-    for col in STR_COLS:
+    for col in STR_COLS + UPPER_STR_COLS:
         if col in df.columns:
             if pd.api.types.is_numeric_dtype(df[col]):
                 report["errors"].append(
