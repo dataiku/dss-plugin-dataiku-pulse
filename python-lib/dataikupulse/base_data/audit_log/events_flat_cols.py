14a614c41bd38317e7ea266ade1f2522bf831c15
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-18 21:24:34 +0000

    Edited file 'python-lib/dataikupulse/base_data/audit_log/events_flat_cols.py'

---
+++ python-lib/dataikupulse/base_data/audit_log/events_flat_cols.py
@@ -0,0 +1,31 @@
+FLAT_COLUMNS = {
+    # Event identity / classification
+    "severity",
+    "logger",
+    "topic",
+    "audittopic",
+    "msgtype",
+    "msgtypebase",
+    "dataiku_category",
+
+    # Actor / auth context
+    "login",
+    "authsource",
+    "authvia",
+    "user",
+
+    # Request / network
+    "callpath",
+    "clientip",
+    "originalip",
+    "xforwardedfor",
+
+    # Time
+    "timestamp",
+    "date",
+
+    # Instance
+    "instance_name",
+    "project_key",
+}
+
