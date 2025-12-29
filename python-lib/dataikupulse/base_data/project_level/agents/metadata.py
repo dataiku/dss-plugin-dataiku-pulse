df8eb71bfdc279c83a592a2926f9eedacef0b2e0
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 21:00:58 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/agents/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/agents/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/agents/metadata.py
@@ -1,25 +1,6 @@
 import pandas as pd
 from dataikupulse.src import dss_funcs
 
-FLAT_COLUMNS = {
-    # Agent identity
-    "project_key",
-    "agents_id",
-    "agents_name",
-    "agents_type",
-
-    # Versioning / lifecycle
-    "agents_activeVersion",
-    "agents_versions_versionId",
-
-    # Version metadata (promoted for queryability)
-    "agents_versions_versionTag_versionNumber",
-    "agents_versions_versionTag_lastModifiedOn",
-    "agents_versions_versionTag_lastModifiedBy_login",
-    "agents_versions_creationTag_versionNumber",
-    "agents_versions_creationTag_lastModifiedOn",
-    "agents_versions_creationTag_lastModifiedBy_login",
-}
 
 def main(self, project_handle, client_d = {}):
     try:
