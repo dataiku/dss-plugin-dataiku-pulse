28abad75d940b74ccbf5907efb91ca6dce17c0ed
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 20:59:32 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/scenarios/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/scenarios/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/scenarios/metadata.py
@@ -1,28 +1,7 @@
 import pandas as pd
 from dataikupulse.src import dss_funcs
 
-FLAT_COLUMNS = {
-    # Scenario identity
-    "project_key",
-    "scenarios_id",
-    "scenarios_name",
 
-    # Classification / state
-    "scenarios_type",
-    "scenarios_active",
-    "scenarios_unavailable",
-    "scenarios_running",
-    "scenarios_markedAsTest",
-
-    # Lifecycle / scheduling
-    "scenarios_createdOn",
-    "scenarios_lastModifiedOn",
-    "scenarios_start",
-    "scenarios_nextRun",
-
-    # Execution context
-    "scenarios_runAsUser",
-}
 
 def main(self, project_handle, client_d = {}):
     try:
@@ -33,5 +12,4 @@ def main(self, project_handle, client_d = {}):
     prefix = "scenarios_"
     df = pd.json_normalize(project_handle.list_scenarios()).add_prefix(prefix)
     df = dss_funcs.rename_and_move_first(df, f"{prefix}projectKey", "project_key")
-    df = dss_funcs.normalize_dataframe(self, df, FLAT_COLUMNS)
     return df
