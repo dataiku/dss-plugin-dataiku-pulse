5dfe7e4f629f2be7f47459cfaa53b7ba719f06c7
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 21:04:05 +0000

    Edited file 'python-lib/dataikupulse/src/schemas/instance_instance_info.py'

---
+++ python-lib/dataikupulse/src/schemas/instance_instance_info.py
@@ -0,0 +1,24 @@
+FLAT_COLUMNS = {
+    # Identity
+    "nodeId",
+    "nodeName",
+    "nodeType",
+    "rawNodeType",
+    "hostname",
+
+    # Instance / License
+    "installId",
+    "dipInstanceId",
+    "licenseInstanceId",
+    "licenseId",
+
+    # Platform / Versioning
+    "dssVersion",
+    "os",
+    "osVersion",
+    "javaVendor",
+    "javaVersion",
+
+    # Time (convert upstream if possible)
+    "dssStartupTimestamp",
+}
