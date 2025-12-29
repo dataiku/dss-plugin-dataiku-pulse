f000aaa55e07a3be09a8b408a8843541e91d0dbc
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 21:03:13 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/llms/metadata.py'

--- python-lib/dataikupulse/base_data/instance_level/llms/metadata.py
+++ python-lib/dataikupulse/base_data/instance_level/llms/metadata.py
@@ -11,5 +11,4 @@ def main(self):
         return pd.DataFrame()
     prefix = "llms"
     df = pd.json_normalize(project_handle.list_llms()).add_prefix(f"{prefix}_")
-    df = dss_funcs.normalize_dataframe(self, df, FLAT_COLUMNS)
     return df
