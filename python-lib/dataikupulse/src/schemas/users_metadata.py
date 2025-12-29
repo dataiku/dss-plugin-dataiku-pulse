6019ee7a405792c5e31da1824b1c3cec234d1a37
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 21:01:49 +0000

    Edited file 'python-lib/dataikupulse/src/schemas/users_metadata.py'

---
+++ python-lib/dataikupulse/src/schemas/users_metadata.py
@@ -0,0 +1,18 @@
+FLAT_COLUMNS = {
+    # Identity
+    "login",
+    "displayName",
+    "email",
+
+    # Account / profile
+    "userProfile",
+    "resultingUserProfile",
+    "enabled",
+    "sourceType",
+
+    # Lifecycle / activity timestamps
+    "creationDate",
+    "last_session_activity",
+    "first_commit_date",
+    "last_commit_date",
+}
