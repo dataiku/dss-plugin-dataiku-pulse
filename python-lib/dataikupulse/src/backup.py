3d0bfdb62f354e5da3e6ffb5b9c4cd093743226b
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 22:07:45 +0000

    Edited file 'python-lib/dataikupulse/src/backup.py'

--- python-lib/dataikupulse/src/backup.py
+++ python-lib/dataikupulse/src/backup.py
@@ -68,3 +68,44 @@ def run_modules(self, mode = "instance", project_handle = None, client_d = {}, p
             except Exception as e:
                 results.append([project_key, category, module_name, f"write/save -- QUALITY", False, e])
     return results
+
+
+
+
+
+# lets split the df by category and save
+        for category, grp_df in merged_df.groupby("dataiku_category"):
+            grp_df = grp_df.dropna(axis=1, how='all').reset_index(drop=True)
+            # Get Flat Columns and Normalize
+            FLAT_COLUMNS = event_flat_cols.get_flat_cols(category)
+            grp_df = dss_silver.normalize_dataframe(self, grp_df, FLAT_COLUMNS)
+            # Order the DF
+            ordered = [c for c in FLAT_COLUMNS if c in grp_df.columns]
+            rest = [c for c in grp_df.columns if c not in ordered]
+            grp_df = grp_df[ordered + rest]
+            try:
+                file_name = f"data-{dt_epoch}.parquet" 
+                write_path = f"raw/dataiku_usage/{category}/{instance_name}/{dt_year}/{dt_month}/{dt_day}/{file_name}"
+                dss_folder.write_remote_folder_output(self, write_path, grp_df)
+                results.append([f"write/save - Dataiku Usage {category}", True, f"data-{dt_epoch}.parquet"])
+            except Exception as e:
+                results.append([f"write/save - Dataiku Usage {category}", False, e])
+
+            # Test quality control -- Save to Silver or Raw Error
+            try:
+                layer = "silver"
+                grp_df = dss_silver.coerce_schema(grp_df)
+                dq = dss_silver.data_quality(grp_df)
+                if dq["errors"]:
+                    layer = "raw_errors"
+                    write_path = f"{layer}/dataiku_usage/{category}/{instance_name}/{dt_year}/{dt_month}/{dt_day}/{file_name}"
+                    dss_folder.write_remote_folder_output(self, write_path, grp_df)
+                    write_path = f"{layer}/dataiku_usage/{category}/{instance_name}/{dt_year}/{dt_month}/{dt_day}/dq_{file_name}"
+                    dss_folder.write_remote_folder_output(self, write_path, pd.DataFrame(dq))
+                else:
+                    write_path = f"{layer}/dataiku_usage/{category}/{instance_name}/{dt_year}/{dt_month}/{dt_day}/{file_name}"
+                    dss_folder.write_remote_folder_output(self, write_path, grp_df)
+                results.append([f"write/save - Dataiku Usage {category} -- {layer}", True, None])
+            except Exception as e:
+                layer = "raw_errors"
+                results.append([f"write/save - Dataiku Usage {category} -- QUALITY", False, e])
