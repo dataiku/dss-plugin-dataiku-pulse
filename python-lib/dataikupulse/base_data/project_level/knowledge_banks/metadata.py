fcee50b0b4ad3b221259b7ed5ef5de3942ed0481
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 21:00:32 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/knowledge_banks/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/knowledge_banks/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/knowledge_banks/metadata.py
@@ -1,31 +1,6 @@
 import pandas as pd
 from dataikupulse.src import dss_funcs
 
-FLAT_COLUMNS = {
-    # Knowledge bank identity
-    "project_key",
-    "knowledge_banks_id",
-    "knowledge_banks_name",
-
-    # Core classification
-    "knowledge_banks_retrieverType",
-    "knowledge_banks_vectorStoreType",
-    "knowledge_banks_embeddingLLMId",
-
-    # Execution / environment
-    "knowledge_banks_rebuildBehavior",
-    "knowledge_banks_envSelection_envMode",
-    "knowledge_banks_envSelection_envName",
-    "knowledge_banks_containerExecSelection_containerMode",
-
-    # Versioning / lifecycle
-    "knowledge_banks_versionTag_versionNumber",
-    "knowledge_banks_versionTag_lastModifiedOn",
-    "knowledge_banks_versionTag_lastModifiedBy.login",
-    "knowledge_banks_creationTag_versionNumber",
-    "knowledge_banks_creationTag_lastModifiedOn",
-    "knowledge_banks_creationTag_lastModifiedBy_login",
-}
 
 
 def main(self, project_handle, client_d = {}):
