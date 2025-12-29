38adc937d42abc80e7f6d719f69d2dc4052327a6
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 17:14:33 +0000

    Edited file 'python-lib/dataikupulse/src/dss_silver.py'

--- python-lib/dataikupulse/src/dss_silver.py
+++ python-lib/dataikupulse/src/dss_silver.py
@@ -79,6 +79,11 @@ NON_STRING_COLS = (
     | set(BOOL_COLS)
 )
 
+def get_string_cols(df: pd.DataFrame) -> list[str]:
+    return [
+        col for col in df.columns
+        if col not in NON_STRING_COLS
+    ]
 
 def coerce_extras_to_json(series: pd.Series) -> pd.Series:
     def to_json_safe(val):
