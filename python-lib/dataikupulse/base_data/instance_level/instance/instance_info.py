0af8a8600b26ccc28bcd358b1ef256f3a87ea0d4
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 21:03:49 +0000

    Edited file 'python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py'

--- python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
+++ python-lib/dataikupulse/base_data/instance_level/instance/instance_info.py
@@ -4,5 +4,4 @@ from dataikupulse.src import dss_funcs
 
 def main(self):
     df = pd.json_normalize(self.local_client.get_instance_info().raw)
-    df = dss_funcs.normalize_dataframe(self, df, FLAT_COLUMNS)
     return df
