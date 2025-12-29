56a2e44d990ae5ecb752feef286b6dcc64eb5e8a
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-29 20:59:52 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/recipes/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/recipes/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/recipes/metadata.py
@@ -1,31 +1,6 @@
 import pandas as pd
 from dataikupulse.src import dss_funcs
 
-FLAT_COLUMNS = {
-    # Recipe identity
-    "project_key",
-    "recipes_name",
-    "recipes_type",
-
-    # Core behavior
-    "recipes_neverRecomputeExistingPartitions",
-    "recipes_redispatchPartitioning",
-    "recipes_maxRunningActivities",
-    "recipes_hashPropagationBehavior",
-
-    # Engine classification
-    "recipes_params_engineType",
-    "recipes_params_engineLabel",
-    "recipes_params_engineRecommended",
-
-    # Versioning / lifecycle
-    "recipes_versionTag_versionNumber",
-    "recipes_versionTag_lastModifiedOn",
-    "recipes_versionTag_lastModifiedBy_login",
-    "recipes_creationTag_versionNumber",
-    "recipes_creationTag_lastModifiedOn",
-    "recipes_creationTag_lastModifiedBy_login",
-}
 
 def add_columns(df, column_to_move, target_column):
     moved_column = df.pop(column_to_move)
