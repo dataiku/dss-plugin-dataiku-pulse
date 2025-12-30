ebbbd0d7861c54446a9b700f4612602fa56a8366
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 15:52:10 +0000

    Edited file 'python-runnables/initialize-silver-layer/runnable.py'

--- python-runnables/initialize-silver-layer/runnable.py
+++ python-runnables/initialize-silver-layer/runnable.py
@@ -31,7 +31,7 @@ class MyRunnable(Runnable):
 
     def silver_instance_projects(self, df, path, results):
         category = path.split("/")[2]
-        module = path.split("/")[3]
+        module_name = path.split("/")[3]
         self.instance_name = path.split("/")[4]
         df_clean, dq = dss_funcs._normalize_and_validate(self, df, category, module_name)
         if dq is None:
