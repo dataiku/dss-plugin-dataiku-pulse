910beb1aaf1f1e92b6bb721d9f8d4f51c3bf855c
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 17:31:25 +0000

    Edited file 'python-lib/dataikupulse/src/dss_funcs.py'

--- python-lib/dataikupulse/src/dss_funcs.py
+++ python-lib/dataikupulse/src/dss_funcs.py
@@ -223,13 +223,13 @@ def _normalize_and_validate(self, df, category, module_name,):
             return None, {"stage": "normalize", "error": e}
     elif flat_cols == False:
         raise Exception(f"Failed to find FLAT COLUMNS file! {category}_{module_name}.py")
+    raise Exception(df.head(1).T)
     # Schema
     try:
         df = dss_silver.coerce_schema(df)
     except Exception as e:
         return None, {"stage": "coerce_schema", "error": e}
     #raise Exception(df.dtypes)
-    raise Exception(df.head(1).T)
     # Quality
     try:
         dq = dss_silver.data_quality(df)
