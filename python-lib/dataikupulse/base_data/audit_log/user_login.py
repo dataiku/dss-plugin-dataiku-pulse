06c7fa2fe6ab05e3eeafa8d7639cf22028259c98
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 20:35:25 +0000

    Edited file 'python-lib/dataikupulse/base_data/audit_log/user_login.py'

--- python-lib/dataikupulse/base_data/audit_log/user_login.py
+++ python-lib/dataikupulse/base_data/audit_log/user_login.py
@@ -88,17 +88,11 @@ def get_developing_users(df):
     return set(action_df["message_login"].dropna().unique())
 
 def classify_users_by_activity(df):
-    """
-    Classifies users as VIEW or DEVELOPING for a single partition.
-    """
-
     view_users = get_view_users(df)
     developing_users = get_developing_users(df)
-
     # DEVELOPING overrides VIEW
     developing_users = developing_users & view_users
     view_only_users = view_users - developing_users
-
     return {
         "developing_users": developing_users,
         "view_users": view_only_users,
