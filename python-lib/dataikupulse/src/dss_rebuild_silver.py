150403dfc9b9779cf6aadfd59b76d53eeef09cc1
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 19:42:09 +0000

    Edited file 'python-lib/dataikupulse/src/dss_rebuild_silver.py'

--- python-lib/dataikupulse/src/dss_rebuild_silver.py
+++ python-lib/dataikupulse/src/dss_rebuild_silver.py
@@ -79,7 +79,6 @@ def rebuild_silver(self, chunk_df):
             mode="operating_system"
         else:
             mode="client"
-            results = silver_instance_projects(self, df, path, results)
         #
         df_clean, dq = dss_funcs._normalize_and_validate(self, df, category, module_name, mode)
     return results
