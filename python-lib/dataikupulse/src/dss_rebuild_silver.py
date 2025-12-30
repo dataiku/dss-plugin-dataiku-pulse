164641a03f5c1e1cc8e90da58cb4676043a793ae
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 19:18:07 +0000

    Edited file 'python-lib/dataikupulse/src/dss_rebuild_silver.py'

--- python-lib/dataikupulse/src/dss_rebuild_silver.py
+++ python-lib/dataikupulse/src/dss_rebuild_silver.py
@@ -50,6 +50,7 @@ def silver_dataiku_usage(self, df, path, results):
     results = check_save(self, df_clean, dq, path, category, None, results)
     return results
 
+
 def silver_instance_projects(self, df, path, results):
     category = path.split("/")[2]
     module_name = path.split("/")[3]
@@ -57,6 +58,7 @@ def silver_instance_projects(self, df, path, results):
     df_clean, dq = dss_funcs._normalize_and_validate(self, df, category, module_name)
     results = check_save(self, df_clean, dq, path, category, module_name, results)
     return results
+
 
 def rebuild_silver(self, chunk_df):
     paths = []
