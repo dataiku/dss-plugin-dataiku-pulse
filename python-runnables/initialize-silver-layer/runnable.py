727e1a410dc9ac1ac0e22deeeca40ee1523946da
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 19:03:40 +0000

    Edited file 'python-runnables/initialize-silver-layer/runnable.py'

--- python-runnables/initialize-silver-layer/runnable.py
+++ python-runnables/initialize-silver-layer/runnable.py
@@ -29,27 +29,7 @@ class MyRunnable(Runnable):
         
     def get_progress_target(self):
         return None
-
-    
-        
-    def rebuild_silver(self, chunk_df):
-        # Get all the partition paths
-        paths = []
-        for row in chunk_df.itertuples():
-            paths.extend(self.folder.get_partition_info(row.partitions)["paths"])
-        # Grab the parquet file and load as a df
-        results = []
-        for path in paths: # Loop paths
-            # Grab the parquet file and load as a df
-            with self.folder.get_download_stream(path) as stream:
-                file_bytes = io.BytesIO(stream.read())
-            df = pd.read_parquet(file_bytes)
-            # Figure out which method to perform for NCQ
-            if "/dataiku_usage/" in path:
-                print(1)
-            else:
-                results = dss_rebuild_silver.silver_instance_projects(self, df, path, results)
-        return results
+       
         
     def run(self, progress_callback):
         # variables
@@ -76,7 +56,7 @@ class MyRunnable(Runnable):
                 batch_size=1,
                 verbose=10,
             )(
-                delayed(self.rebuild_silver)(chunk)
+                delayed(dss_rebuild_silver.rebuild_silver)(self, chunk)
                 for chunk in chunks
                 if not chunk.empty
             )
@@ -85,7 +65,7 @@ class MyRunnable(Runnable):
                 dfs.append(pd.DataFrame(r).astype(str))
             results_df = pd.concat(dfs, ignore_index=True)
         else:
-            results = self.rebuild_silver(partition_df)
+            results = dss_rebuild_silver.rebuild_silver(self, partition_df)
             results_df =pd.DataFrame(results).astype(str)
             
         # Return ResultsTable
