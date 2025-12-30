5f830be63b9c0975c8349e86a96b05c9c73a18f2
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 19:13:00 +0000

    Edited file 'python-lib/dataikupulse/src/dss_rebuild_silver.py'

--- python-lib/dataikupulse/src/dss_rebuild_silver.py
+++ python-lib/dataikupulse/src/dss_rebuild_silver.py
@@ -5,8 +5,43 @@ import pandas as pd
 from dataikupulse.src.schemas import audit_dataiku_usage
 from dataikupulse.src import dss_folder, dss_funcs, dss_silver
 
-def check_save():
-    return
+def check_save(self, df, path, results, df_clean, dq):
+    if dq is None:
+        results.append([category, module_name, "quality", False, "Unknown failure"])
+        return results
+    if df_clean is None:
+        results.append([
+            category,
+            module_name,
+            f"quality -- {dq['stage']}",
+            False,
+            dq["error"],
+        ])
+        return results
+    df_report = pd.DataFrame([{
+        "errors": dq["errors"],
+        "warnings": dq["warnings"],
+        **dq["stats"],
+    }])
+    base_path = path.replace("/raw/", "")
+    if dq["errors"]:
+        write_path = f"/raw_errors/{base_path}"
+    else:
+        write_path = f"/silver/{base_path}"
+    dss_folder.write_remote_folder_output(self, write_path, df_clean)
+    if dq["errors"]:
+        filename = os.path.basename(write_path)
+        report_path = write_path.replace(filename, f"dq_{filename}")
+        df_report = pd.DataFrame([{
+            "errors": dq["errors"],
+            "warnings": dq["warnings"],
+            **dq["stats"],
+        }])
+        dss_folder.write_remote_folder_output(self, report_path, df_report)
+        results.append([category, module_name, f"write/save -- raw_errors", False, "Check raw errors"])
+    else:
+        results.append([category, module_name, f"write/save -- silver", True, None])
+    return results
     
 
 def silver_dataiku_usage(self, df, path, results):
