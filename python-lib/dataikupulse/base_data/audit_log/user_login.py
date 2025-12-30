f908d191c6ddf8e93c314099c9bfc264b6642c93
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-30 22:18:19 +0000

    Edited file 'python-lib/dataikupulse/base_data/audit_log/user_login.py'

--- python-lib/dataikupulse/base_data/audit_log/user_login.py
+++ python-lib/dataikupulse/base_data/audit_log/user_login.py
@@ -135,7 +135,6 @@ def main(self, df):
         try:
             # [project_key, category, module_name, "write/save -- RAW", True, None]
             long_results = dss_funcs._persist_raw(self, df, "users", "user_login_acivity", None, [])
-            raise Exception(long_results)
             long_results = dss_funcs._process_quality_and_persist(self, df, category, module_name, project_key, "SKIP", f"data-{dt_epoch}.parquet", [])
             results.append(["User Login Classification", "write/save", True, f"{dt_epoch}"])
         except Exception as e:
