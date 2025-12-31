9fd3f06fb8b967304766a0bd1e3de4853448518e
smazzei <Stephen.Mazzei@dataiku.com>
2025-12-31 14:04:26 +0000

    Edited file 'python-runnables/initialize-silver-layer/runnable.py'

--- python-runnables/initialize-silver-layer/runnable.py
+++ python-runnables/initialize-silver-layer/runnable.py
@@ -44,7 +44,7 @@ class MyRunnable(Runnable):
         partitions_df = partitions_df.loc[partitions_df["layer"] == "raw"]
         
         # added filtering
-        #partitions_df = partitions_df.loc[partitions_df["category"] == "dataiku_usage"]
+        partitions_df = partitions_df.loc[partitions_df["category"] == "dataiku_usage"]
         
         # Re-Run Silver Quality Guard
         if self.preset_pc["do_parallel"]:
