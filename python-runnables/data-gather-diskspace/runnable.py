65cef2680a37eb429cff4a76261c6f4d91420250
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-06 15:33:04 +0000

    Edited file 'python-runnables/data-gather-diskspace/runnable.py'

--- python-runnables/data-gather-diskspace/runnable.py
+++ python-runnables/data-gather-diskspace/runnable.py
@@ -111,45 +111,16 @@ class MyRunnable(Runnable):
                 file_name=f"data.parquet", 
                 result=[]
             )
-            results.append(["Operating System - Diskspace", "write/save - RAW", True, None])
+            results.append(["write/save - RAW", True, None])
         except Exception as e:
-            results.append(["Operating System - Diskspace", "write/save - RAW", False, e])
+            results.append(["write/save - RAW", False, e])
         # SILVER
         try:
             long_results = dss_funcs._process_quality_and_persist(self, df, "users", "user_login_activity", None, "SKIP", f"data-{dt_epoch}.parquet", [])
-            results.append(["Operating System - Diskspace", "write/save - SILVER", True, None])
+            results.append(["write/save - SILVER", True, None])
         except Exception as e:
-            results.append(["Operating System - Diskspace", "write/save - SILVER", False, e])
+            results.append(["write/save - SILVER", False, e])
         
-        
-        
-        
-        try:
-            write_path = f"raw/operating_system/diskspace/{instance_name}/{dt_year}/{dt_month}/{dt_day}/data.parquet"
-            dss_folder.write_remote_folder_output(self, write_path, df)
-            results.append(["write/save", True, None])
-        except Exception as e:
-            results.append(["write/save", False, e])
-        
-        # Test quality control -- Save to Silver or Raw Error
-        try:
-            layer = "silver"
-            df = dss_silver.coerce_schema(df)
-            dq = dss_silver.data_quality(df)
-            if dq["errors"]:
-                layer = "raw_errors"
-                write_path = f"{layer}/operating_system/diskspace/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/data.parquet"
-                dss_folder.write_remote_folder_output(self, write_path, df)
-                write_path = f"{layer}/operating_system/diskspace/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/dq_data.parquet"
-                dss_folder.write_remote_folder_output(self, write_path, pd.DataFrame(dq))
-            else:
-                write_path = f"{layer}/operating_system/diskspace/{self.instance_name}/{dt_year}/{dt_month}/{dt_day}/data.parquet"
-                dss_folder.write_remote_folder_output(self, write_path, df)
-            results.append([f"write/save -- {layer}", True, None])
-        except Exception as e:
-            layer = "raw_errors"
-            results.append([f"write/save -- QUALITY", False, e])
-            
         # return results
         if results:
             df = pd.DataFrame(results, columns=["step", "result", "message"])
