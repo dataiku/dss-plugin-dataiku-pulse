2ea5e25051833b3d2149f2871797992770a9980b
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-06 15:05:32 +0000

    Edited file 'python-lib/dataikupulse/src/schemas/operating_system_diskspace.py'

---
+++ python-lib/dataikupulse/src/schemas/operating_system_diskspace.py
@@ -0,0 +1,11 @@
+FLAT_COLUMNS = [
+    # Identity
+    "llms_id",
+    "llms_friendlyName",
+    "llms_friendlyNameShort",
+
+    # Provider / Classification
+    "llms_type",
+    "llms_connection",
+    "llms_model",
+]
