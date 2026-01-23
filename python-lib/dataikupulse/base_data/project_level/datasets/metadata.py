b31b71e0dc55d109a8025450a5634338e149ffd1
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-23 16:49:10 +0000

    Edited file 'python-lib/dataikupulse/base_data/project_level/datasets/metadata.py'

--- python-lib/dataikupulse/base_data/project_level/datasets/metadata.py
+++ python-lib/dataikupulse/base_data/project_level/datasets/metadata.py
@@ -13,4 +13,3 @@ def main(self, project_handle, client_d = {}):
     df = dss_funcs.rename_and_move_first(df, f"{prefix}projectKey", "project_key")
     df = dss_funcs._trim_raw_df_by_last_modified(df, last_modified_col="dataset_versionTag.lastModifiedOn", last_update=self.last_update)
     return df
-
