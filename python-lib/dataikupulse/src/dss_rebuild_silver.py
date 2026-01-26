795027e5f301b01ae57d02b6fe9717672f4e6d2c
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-26 19:19:31 +0000

    Edited file 'python-lib/dataikupulse/src/dss_rebuild_silver.py'

--- python-lib/dataikupulse/src/dss_rebuild_silver.py
+++ python-lib/dataikupulse/src/dss_rebuild_silver.py
@@ -50,7 +50,7 @@ def rebuild_silver(self, chunk_df):
     #
     results = []
     for path in paths:
-        if "=" is not in path:
+        if "=" not in path:
             continue
         #
         with self.folder.get_download_stream(path) as stream:
