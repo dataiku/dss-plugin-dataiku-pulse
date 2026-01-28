8cf8a98641532f0622cc74ddbc0820baa550c425
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-28 19:08:41 +0000

    Edited file 'custom-recipes/create-gold-tables/recipe.py'

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -1,18 +1,8 @@
-import dataiku
 from dataiku.customrecipe import get_input_names_for_role
 from dataiku.customrecipe import get_output_names_for_role
 from dataiku.customrecipe import get_recipe_config
-
-#partitioned_data_folder = get_output_names_for_role('partitioned_data_folder')
-#pdf = [dataiku.Dataset(name) for name in partitioned_data_folder]
-
-my_variable = get_recipe_config().get('parameter_name', None)
-
-
-
 from dataiku.customrecipe import get_plugin_config
 plugin_config = get_plugin_config()
-
 
 ####################################################################################################################
 # -*- coding: utf-8 -*-
