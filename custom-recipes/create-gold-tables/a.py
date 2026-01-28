a18d0ddc78d5155ac01680b607979346251171c2
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-28 19:06:45 +0000

    Edited file 'custom-recipes/create-gold-tables/a.py'

---
+++ custom-recipes/create-gold-tables/a.py
@@ -0,0 +1,25 @@
+"""
+{
+    'pulse_repo_url': 'https://github.com/dataiku/dss-plugin-dataiku-pulse.git',
+    'pulse_repo_branch': 'mazzei-dev-2.1',
+    'pulse_project_key': 'DATAIKU_PULSE',
+    'pulse_worker_key': 'DATAIKU_PULSE_WORKER',
+    'pulse_dataiku_user': 'smazzei',
+    'ignore_certs': True,
+    'do_parallel': True,
+    'cores': 2,
+    'api_configs': [
+        {
+            'worker_url': 'https://mazzei-designer.fe-aws.dkucloud-dev.com',
+            'worker_api': 'dkuaps-asdf'
+        },
+        {
+            'worker_url': 'https://tam-global.fe-aws.dkucloud-dev.com',
+            'worker_api': 'dkuaps-asfd'
+        }
+    ],
+    'pulse_project_url': 'https://mazzei-designer.fe-aws.dkucloud-dev.com',
+    'pulse_folder_connection': 'mazzei-s3-bucket',
+    'pulse_project_api': 'dkuaps-asdf'
+}
+"""
