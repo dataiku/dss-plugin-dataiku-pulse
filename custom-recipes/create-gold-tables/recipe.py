065ce984e789e635f3f0605d5f661f14cd5534f4
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-28 20:27:33 +0000

    Edited file 'custom-recipes/create-gold-tables/recipe.py'

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -11,9 +11,9 @@ from backend.duck_db import create_conn
 def build_gold_tables():
     create_conn.reset_duckdb(reset=True)
     conn = create_conn.create_connection(read_only=False)
-    expand_duckdb.configure_duckdb_runtime(conn),
-    dataiku_sources.register_partition_df(conn),
-    raw_views.register_raw_views(conn),
-    dataiku_usage.register_dataiku_usage_views(conn),
-    gold_tables.register_gold_tables(conn),
+    expand_duckdb.configure_duckdb_runtime(conn)
+    dataiku_sources.register_partition_df(conn)
+    raw_views.register_raw_views(conn)
+    dataiku_usage.register_dataiku_usage_views(conn)
+    gold_tables.register_gold_tables(conn)
     return
