41f6b80893a2b75e9ca96bfc1417388cb71e6aab
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-28 19:40:01 +0000

    Edited file 'custom-recipes/create-gold-tables/recipe.py'

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -9,13 +9,14 @@ from backend.duck_db import create_conn
 
 
 def build_gold_tables():
+    create_conn.reset_duckdb(reset=True)
     try:
-        create_conn.reset_duckdb(reset=True)
-        expand_duckdb.configure_duckdb_runtime(),
-        dataiku_sources.register_partition_df(),
-        raw_views.register_raw_views(),
-        dataiku_usage.register_dataiku_usage_views(),
-        gold_tables.register_gold_tables(),
+        conn = create_conn.create_connection(read_only=False)
+        expand_duckdb.configure_duckdb_runtime(conn),
+        dataiku_sources.register_partition_df(conn),
+        raw_views.register_raw_views(conn),
+        dataiku_usage.register_dataiku_usage_views(conn),
+        gold_tables.register_gold_tables(conn),
     except Exception as e:
         raise Exception(e)
     return
