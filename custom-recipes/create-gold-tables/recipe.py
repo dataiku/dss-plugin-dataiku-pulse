003181d86a71dd3579c9acb2938aabdee27f6049
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-28 19:06:49 +0000

    Edited file 'custom-recipes/create-gold-tables/recipe.py'

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -12,31 +12,7 @@ my_variable = get_recipe_config().get('parameter_name', None)
 
 from dataiku.customrecipe import get_plugin_config
 plugin_config = get_plugin_config()
-"""
-{
-    'pulse_repo_url': 'https://github.com/dataiku/dss-plugin-dataiku-pulse.git',
-    'pulse_repo_branch': 'mazzei-dev-2.1',
-    'pulse_project_key': 'DATAIKU_PULSE',
-    'pulse_worker_key': 'DATAIKU_PULSE_WORKER',
-    'pulse_dataiku_user': 'smazzei',
-    'ignore_certs': True,
-    'do_parallel': True,
-    'cores': 2,
-    'api_configs': [
-        {
-            'worker_url': 'https://mazzei-designer.fe-aws.dkucloud-dev.com',
-            'worker_api': 'dkuaps-MSleUwXxL2lCXEMQZVwiJNjMHfS0zIJ2'
-        },
-        {
-            'worker_url': 'https://tam-global.fe-aws.dkucloud-dev.com',
-            'worker_api': 'dkuaps-VcHO32hIj9tg8pFrMBnmirM340uE1rOC'
-        }
-    ],
-    'pulse_project_url': 'https://mazzei-designer.fe-aws.dkucloud-dev.com',
-    'pulse_folder_connection': 'mazzei-s3-bucket',
-    'pulse_project_api': 'dkuaps-MSleUwXxL2lCXEMQZVwiJNjMHfS0zIJ2'
-}
-"""
+
 
 ####################################################################################################################
 # -*- coding: utf-8 -*-
