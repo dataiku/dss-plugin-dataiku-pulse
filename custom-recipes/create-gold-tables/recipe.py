aee3cdf50f4b5d2a2dc542bfe6b046dd97a28740
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-28 19:15:36 +0000

    Edited file 'custom-recipes/create-gold-tables/recipe.py'

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -5,6 +5,6 @@ from dataiku.customrecipe import get_plugin_config
 plugin_config = get_plugin_config()
 
 ####################################################################################################################
-from duckdb import settings
+from duckdb_handle import settings
 print("####################################################################################################################")
 print(settings.DB_PATH)
