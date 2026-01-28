9832ac99d932a0da1e1ebfebf2d285a8e0271041
smazzei <Stephen.Mazzei@dataiku.com>
2026-01-28 19:22:53 +0000

    Edited file 'python-lib/backend/duckdb/init_duckdb.py'

--- python-lib/backend/duckdb/init_duckdb.py
+++ python-lib/backend/duckdb/init_duckdb.py
@@ -2,7 +2,7 @@ from duckdb_handle import create_conn
 
 def build_gold_tables():
     try:
-        create_conn.reset_duckdb(reset=reset)
+        create_conn.reset_duckdb(reset=True)
     except Exception as e:
         raise Exception(e)
     return
