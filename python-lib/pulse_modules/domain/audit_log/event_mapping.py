0c242bbf7291db6b44a6f4ba0a7cf81595b41a5e
admin <admin>
2026-02-03 20:41:56 +0000

    Edited file 'python-lib/pulse_modules/domain/audit_log/event_mapping.py'

--- python-lib/pulse_modules/domain/audit_log/event_mapping.py
+++ python-lib/pulse_modules/domain/audit_log/event_mapping.py
@@ -85,14 +85,14 @@ def main(self, df):
             # RAW 
             try:
                 long_results = dss_funcs._persist_raw(self, mn_df, category, module_name, None, file_name, [])
-                results.append([category, f"write/save - {module_name} -- raw", True, file_name])
+                results.append([category, f"write/save - {module_name} -- raw", True, None])
             except Exception as e:
                 results.append([category, f"write/save - {module_name} -- raw", False, e])
                 continue
             # SILVER
             try:
                 long_results = dss_funcs._process_quality_and_persist(self, mn_df, category, module_name, None, category, file_name, [])
-                results.append([category, f"write/save - {module_name} -- silver", True, file_name])
+                results.append([category, f"write/save - {module_name} -- silver", True, None])
             except Exception as e:
                 results.append([category, f"write/save - {module_name} -- silver", False, e])
     return results
