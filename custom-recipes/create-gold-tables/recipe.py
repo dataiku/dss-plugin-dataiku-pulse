249e3e8fe7ae90fa2630a552b8d4aff9ee77423a
admin <admin>
2026-02-03 20:43:45 +0000

    Edited file 'custom-recipes/create-gold-tables/recipe.py'

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -1,10 +1,3 @@
-from dataiku.customrecipe import get_input_names_for_role
-from dataiku.customrecipe import get_output_names_for_role
-from dataiku.customrecipe import get_recipe_config
-from dataiku.customrecipe import get_plugin_config
-plugin_config = get_plugin_config()
-
-# -------------------------------------------------------------------------------------------
 from backend.duck_db import create_conn
 from backend.duck_db import expand_duckdb
 from backend.duck_db import dataiku_sources
