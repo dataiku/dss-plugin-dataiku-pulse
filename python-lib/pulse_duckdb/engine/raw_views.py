411e52b2757e36cee3eb1fc14b781cfb31981ec3
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-12 22:31:42 +0000

    Edited file 'python-lib/pulse_duckdb/engine/raw_views.py'

--- python-lib/pulse_duckdb/engine/raw_views.py
+++ python-lib/pulse_duckdb/engine/raw_views.py
@@ -3,7 +3,7 @@ import streamlit as st
 from pulse_duckdb import settings
 from pulse_duckdb.helpers import yaml_loader
 
-logger = logging.getLogger("dataiku_pulse_logger")
+logger = logging.getLogger(__name__)
 queries = yaml_loader.load_yaml(settings.BASE_DIR / "pulse_duckdb/config/raw_views/base_query.yaml")
 
 # -------------------------------------------------------
