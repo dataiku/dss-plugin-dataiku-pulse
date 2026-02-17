3e35fd12787d4f3dcd662c0194a6a1270ed9fe37
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-17 20:05:07 +0000

    Edited files

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -36,11 +36,8 @@ def build_gold_tables():
     base_tables = df['table_name'].tolist()
     
     # 4. Custom edits
-    recipe_config = get_recipe_config()
-    enable_s3_sse_aes256 = recipe_config.get('enable_s3_sse_aes256', False)
-    if enable_s3_sse_aes256:
-        logger.info("Enabling S3 server-side encryption (AES256) for unloads.")
-        conn.execute("SET s3_server_side_encryption='AES256';")
+    #recipe_config = get_recipe_config()
+
     
     # 5. Unload the gold tables
     base_path = f"{settings.blob_header}://{settings.blob_bket}/{settings.dss_gold_tables_folder_root}/gold"
