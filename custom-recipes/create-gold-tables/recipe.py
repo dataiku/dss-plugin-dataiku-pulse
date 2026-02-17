91959ff8f671c8a50cd1bea9f6c688c8b5613f5b
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-17 20:11:52 +0000

    Edited file 'custom-recipes/create-gold-tables/recipe.py'

--- custom-recipes/create-gold-tables/recipe.py
+++ custom-recipes/create-gold-tables/recipe.py
@@ -36,8 +36,8 @@ def build_gold_tables():
     base_tables = df['table_name'].tolist()
     
     # 4. Custom edits
+    #from dataiku.customrecipe import get_recipe_config
     #recipe_config = get_recipe_config()
-
     
     # 5. Unload the gold tables
     base_path = f"{settings.blob_header}://{settings.blob_bket}/{settings.dss_gold_tables_folder_root}/gold"
