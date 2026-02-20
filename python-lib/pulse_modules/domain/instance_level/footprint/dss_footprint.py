6e98f86274744d666da8230089505d049f8d93fe
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-20 16:27:02 +0000

    Edited file 'python-lib/pulse_modules/domain/instance_level/footprint/dss_footprint.py'

--- python-lib/pulse_modules/domain/instance_level/footprint/dss_footprint.py
+++ python-lib/pulse_modules/domain/instance_level/footprint/dss_footprint.py
@@ -1,14 +1,12 @@
 import pandas as pd
 
 
-def save_df(self, df, category, module_name, file_name, results):
+def save_df(self, df, category, module_name, file_name):
     # RAW 
     try:
         long_results = dss_funcs._persist_raw(self, df, category, module_name, None, file_name, [])
-        results.append([category, f"write/save - {module_name} -- raw", True, None])
     except Exception as e:
-        results.append([category, f"write/save - {module_name} -- raw", False, e])
-        continue
+        return
     # SILVER
     try:
         long_results = dss_funcs._process_quality_and_persist(self, df, category, module_name, None, category, file_name, [])
