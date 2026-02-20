80cfbbfffd86bad0a789374ff25a395050e2b38f
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-20 16:56:50 +0000

    Edited file 'python-lib/pulse_modules/helpers/dss_funcs.py'

--- python-lib/pulse_modules/helpers/dss_funcs.py
+++ python-lib/pulse_modules/helpers/dss_funcs.py
@@ -115,8 +115,12 @@ def _dss_footprint(self):
         self.dss_footprint = self.local_client.get_data_directories_footprint().compute_all_dss_footprint()
     except:
         self.dss_footprint = pd.DataFrame()
-    return self
-    
+    return
+
+def _expansion_self_data(self, module_path):
+    if "footprint" in module_path:
+        _dss_footprint(self)
+    return
 
 
 # ----------------------------------------------------------
