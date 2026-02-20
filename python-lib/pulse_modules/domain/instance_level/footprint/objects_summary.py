061d32f2024d5f31b9df7c42b1391b94d4421c6d
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-20 17:20:04 +0000

    Edited file 'python-lib/pulse_modules/domain/instance_level/footprint/objects_summary.py'

--- python-lib/pulse_modules/domain/instance_level/footprint/objects_summary.py
+++ python-lib/pulse_modules/domain/instance_level/footprint/objects_summary.py
@@ -5,41 +5,33 @@ def main(self):
     if self.dss_footprint.empty:
         return pd.DataFrame()
     df = self.dss_footprint.empty
-    
-    # Quick totals
-    totals_cols = ["size", "nbFiles", "nbFolders", "nbErrors"]
-    totals_df = (
-        df[totals_cols]
-        .drop_duplicates()
-        .reset_index(drop=True)
-    )
-    return totals_df
 
-    #####################################################
     # Transpose (drop totals first)
+    totals_cols = ["size", "nbFiles", "nbFolders", "nbErrors"]
     transpose_df = (
         df
         .drop(columns=totals_cols)
         .T
     )
     
-    #####################################################
-    # Summary table
-    summary_df = (
-        transpose_df
-        .drop(columns=["items", "locations"], errors="ignore")
-        .reset_index()
-    )
-    # Save output
-    
-    #####################################################
     # Object-level expansion
+    dfs = []
     for name, row in transpose_df.iterrows():
         if not isinstance(row.get("items"), list):
             continue
         tmp_df = pd.DataFrame(row["items"])
         tmp_df.insert(loc=0, column='object', value=name)
-        # Save output
+        cols = ["object"]
+        if name in ["projects" , "orphanProjects"]:
+            cols += ["projectKey"]
+        elif name in ["codeEnvs", "plugins"]:
+            cols += ["name"]
+        else:
+            self.logger.error(f"Unknown DSS Footprint name: {name}")
+        cols += totals_cols
+        tmp_df = tmp_df[cols]
+        tmp_df.columns = ["object", "name"] + totals_cols
+        dfs.append(tmp_df)
 
     #####################################################
     # END
