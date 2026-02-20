09a4e7dd0e525e976ef2042e7cba0c67546c37b3
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-20 16:21:22 +0000

    Edited file 'python-lib/pulse_modules/domain/instance_level/footprint/short_summary.py'

--- python-lib/pulse_modules/domain/instance_level/footprint/short_summary.py
+++ python-lib/pulse_modules/domain/instance_level/footprint/short_summary.py
@@ -20,18 +20,18 @@ def save_df(df):
         
 def main(self):
     try:
-        data = self.local_client.get_data_directories_footprint().compute_all_dss_footprint()
+        # call API and create DF
+        data = client.get_data_directories_footprint().compute_all_dss_footprint()
         df = pd.DataFrame(data)
     except:
         return pd.DataFrame()
     
-    # Quick totals
-    totals_cols = ["size", "nbFiles", "nbFolders", "nbErrors"]
-    totals_df = (
-        df[totals_cols]
-        .drop_duplicates()
-        .reset_index(drop=True)
-    )
-    if len(totals_df) != 1:
-        return pd.DataFrame()
-    return totals_df
+
+    
+    
+    
+    
+    
+    
+    
+    return pd.DataFrame()
