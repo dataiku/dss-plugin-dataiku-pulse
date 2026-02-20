fc3f1da189bd1f096aab702b942281f30305c7b8
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-20 16:20:35 +0000

    Edited file 'python-lib/pulse_modules/domain/instance_level/footprint/short_summary.py'

--- python-lib/pulse_modules/domain/instance_level/footprint/short_summary.py
+++ python-lib/pulse_modules/domain/instance_level/footprint/short_summary.py
@@ -1,5 +1,22 @@
 import pandas as pd
 
+def save_df(df):
+    # RAW 
+    try:
+        long_results = dss_funcs._persist_raw(self, mn_df, category, module_name, None, file_name, [])
+        results.append([category, f"write/save - {module_name} -- raw", True, None])
+    except Exception as e:
+        results.append([category, f"write/save - {module_name} -- raw", False, e])
+        continue
+    # SILVER
+    try:
+        long_results = dss_funcs._process_quality_and_persist(self, mn_df, category, module_name, None, category, file_name, [])
+        results.append([category, f"write/save - {module_name} -- silver", True, None])
+    except Exception as e:
+        results.append([category, f"write/save - {module_name} -- silver", False, e])
+    return
+
+        
 def main(self):
     try:
         data = self.local_client.get_data_directories_footprint().compute_all_dss_footprint()
