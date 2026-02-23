36d16642f104cd357e19fb33541664a87bdbefa0
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-23 16:41:45 +0000

    Edited file 'python-lib/pulse_modules/flat_columns/users_metadata.py'

--- python-lib/pulse_modules/flat_columns/users_metadata.py
+++ python-lib/pulse_modules/flat_columns/users_metadata.py
@@ -9,10 +9,19 @@ FLAT_COLUMNS = [
     "resultingUserProfile",
     "enabled",
     "sourceType",
+    "groups",
 
     # Lifecycle / activity timestamps
     "creationDate",
     "last_session_activity",
     "first_commit_date",
     "last_commit_date",
+    
+    # trial licenses
+    "trial_exist",
+    "trial_valid",
+    "trial_expired",
+    "trial_illegal",
+    "trial_granted_on",
+    "trial_expires_on",
 ]
