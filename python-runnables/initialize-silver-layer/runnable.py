73e15e79b9b8203ecf0ef029fbf67653c7757e52
smazzei <Stephen.Mazzei@dataiku.com>
2026-02-23 17:01:15 +0000

    Edited file 'python-runnables/initialize-silver-layer/runnable.py'

--- python-runnables/initialize-silver-layer/runnable.py
+++ python-runnables/initialize-silver-layer/runnable.py
@@ -38,11 +38,17 @@ class MyRunnable(Runnable):
         
         # Gather Paths
         all_paths = self.folder.list_paths_in_partition()
-        if self.config["pulse_limit_category"] == "all"
-        paths = [
-            path for path in all_paths
-            if "/raw/category=" in path
-        ]
+        limit = self.config["pulse_limit_category"]
+        if limit == "all":
+            paths = [
+                path for path in all_paths
+                if "/raw/category=" in path
+            ]
+        else:
+            paths = [
+                path for path in all_paths
+                if f"/raw/category={limit}" in path
+            ]
         if not paths:
             raise Exception("No Hive-partitioned paths found")
         
