d87002be43df9118e3383b3b4fda51ff9f2752d9
smazzei <Stephen.Mazzei@dataiku.com>
2026-03-26 15:19:39 +0000

    Edited file 'python-lib/pulse_modules/flat_columns/dashboards_metadata.py'

--- python-lib/pulse_modules/flat_columns/dashboards_metadata.py
+++ python-lib/pulse_modules/flat_columns/dashboards_metadata.py
@@ -1,10 +1,9 @@
 FLAT_COLUMNS = [
-    # Agent Tools identity
     "project_key",
     "dashboard_id",
     "dashboard_name",
     "dashboard_owner",
     "dashboard_lastModifiedBy_login",
     "dashboard_createdOn",
-    "",
+    "dashboard_lastModifiedOn",
 ]
