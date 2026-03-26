8161c034e7a655409d5175a0a46245f2555b7666
smazzei <Stephen.Mazzei@dataiku.com>
2026-03-26 15:25:28 +0000

    Edited file 'python-lib/pulse_modules/flat_columns/webapps_metadata.py'

---
+++ python-lib/pulse_modules/flat_columns/webapps_metadata.py
@@ -0,0 +1,10 @@
+FLAT_COLUMNS = [
+    "project_key",
+    "webapps_id",
+    "webapps_name",
+    "webapps_type",
+    "webapps_createdBy_login",
+    "webapps_lastModifiedBy_login",
+    "webapps_createdOn",
+    "webapps_lastModifiedOn",
+]
