2d1db0f42ad6942499a587bfd449d589151b64a2
smazzei <stephen.mazzei@dataiku.com>
2026-08-27 21:05:28 +0000

    Edited file 'custom-recipes/compact-silver-layer/recipe.py'

--- custom-recipes/compact-silver-layer/recipe.py
+++ custom-recipes/compact-silver-layer/recipe.py
@@ -1,11 +1,24 @@
 import dataiku
-from dataiku.customrecipe import get_recipe_config
+from dataiku.customrecipe import get_recipe_config, get_plugin_config
 
+# Recipe parameters (configured per recipe instance)
+recipe_config = get_recipe_config() or {}
+
+# Global plugin parameters (configured at the plugin settings level)
+
+# Accessing a plugin parameter safely
+global_api_key = plugin_config.get("api_key", "default_val")
 
 def run():
     project_key = dataiku.default_project_key()
+    
     recipe_config = get_recipe_config() or {}
-    normalize_silver = bool(recipe_config.get("normalize_silver", True))
+    plugin_config = get_plugin_config() or {}
+    
+    print(f"AHHHHHHH -- {plugin_config}")
+    print(f"AHHHHHHH -- {recipe_config}")
+    
+    
     return 
 
 if __name__ == "__main__":
